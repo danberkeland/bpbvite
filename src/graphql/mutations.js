ca4f1910df3e3d2c76a150ed4cf4c3bdbcc555d3
Dan Berkeland <danberkeland@gmail.com>
2022-08-11 09:28:14 -0700

    amplify pushed new api

---
+++ src/graphql/mutations.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createTodo2 = /* GraphQL */ `
+  mutation CreateTodo2(
+    $input: CreateTodo2Input!
+    $condition: ModelTodo2ConditionInput
+  ) {
+    createTodo2(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateTodo2 = /* GraphQL */ `
+  mutation UpdateTodo2(
+    $input: UpdateTodo2Input!
+    $condition: ModelTodo2ConditionInput
+  ) {
+    updateTodo2(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteTodo2 = /* GraphQL */ `
+  mutation DeleteTodo2(
+    $input: DeleteTodo2Input!
+    $condition: ModelTodo2ConditionInput
+  ) {
+    deleteTodo2(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
