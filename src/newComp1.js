cc89f1d666409bb62f3a12fe9306afd2e6b01434
Dan Berkeland <danberkeland@gmail.com>
2022-08-11 09:21:05 -0700

    added feature

--- src/newComp1.js
+++ src/newComp1.js
@@ -1,13 +1,19 @@
 
+import React from 'react';
 import './App.css';
+import SubComp1 from './subComp1';
 
 function NewComp1() {
   return (
-    <div className="App">
+    <React.Fragment>
+      <SubComp1 />
+      <div className="App">
      New Comp 1
      made a couple changes
      made a few more changes
     </div>
+    </React.Fragment>
+    
   );
 }
 
