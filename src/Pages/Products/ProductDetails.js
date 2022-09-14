af09480a3b1d5481e19934295e71d788e092ca7c
Dan Berkeland <danberkeland@gmail.com>
2022-09-14 06:08:14 -0700

    added Edit button

--- src/Pages/Products/ProductDetails.js
+++ src/Pages/Products/ProductDetails.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 
-function ProductDetails({ selectedProduct }) {
+import { Button } from "primereact/button";
 
-    const [ edit, setEdit ] = useState(false)
+function ProductDetails({ selectedProduct }) {
+  const [edit, setEdit] = useState(false);
+
+  const buttonStyle = { width: "80px", margin: "20px", fontSize: "1.2em" };
 
   return (
     <React.Fragment>
       {/*<pre>Selected Product: {JSON.stringify(selectedProduct, null, 4)}</pre>*/}
 
-      {!edit ? 
-      <React.Fragment>
-      <h1>{selectedProduct.prodName}</h1>
-      <h2>ID: {selectedProduct.prodNick}</h2>
-      <h3>Wholesale Price: {selectedProduct.wholePrice}</h3>
-      <h3>Pack Size: {selectedProduct.packSize}</h3>
-      </React.Fragment> :
-      <div></div>}
+      {!edit ? (
+        <div className="productDetails">
+          <h1>{selectedProduct.prodName}</h1>
+          <Button
+            label="Edit"
+            className="p-button-raised p-button-rounded"
+            style={buttonStyle}
+          />
+          <h2>ID: {selectedProduct.prodNick}</h2>
+          <h3>Wholesale Price: {selectedProduct.wholePrice}</h3>
+          <h3>Pack Size: {selectedProduct.packSize}</h3>
+        </div>
+      ) : (
+        <div></div>
+      )}
 
       <div className="bottomSpace"></div>
     </React.Fragment>
