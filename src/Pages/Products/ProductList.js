8642927477354c1e52c22a0b5429920060f69e16
Dan Berkeland <danberkeland@gmail.com>
2022-09-14 19:15:12 -0700

    created shell for create product

--- src/Pages/Products/ProductList.js
+++ src/Pages/Products/ProductList.js
@@ -5,7 +5,15 @@ import { FilterMatchMode } from "primereact/api";
 
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Button } from "primereact/button";
 import { grabDetailedProductList } from "../../restAPIs";
+
+const submitButtonStyle = {
+  width: "100px",
+  margin: "20px",
+  fontSize: "1.2em",
+  backgroundColor: "red",
+};
 
 function ProductList({
   selectedProduct,
@@ -18,6 +26,7 @@ function ProductList({
   const [filter, setFilter] = useState({
     prodName: { value: null, matchMode: FilterMatchMode.CONTAINS },
   });
+  const [isCreate, setIsCreate] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -28,28 +37,54 @@ function ProductList({
     });
   }, []);
 
+  const handleClick = () => {
+    setIsCreate(!isCreate);
+  };
+
+  const handleSubmit = () => {
+    setIsCreate(!isCreate);
+  };
+
   return (
     <React.Fragment>
-      <React.Fragment>
-          <button >+ CREATE PRODUCT</button>
-          
+      {!isCreate ? (
+        <React.Fragment>
+          <button onClick={handleClick}>+ CREATE PRODUCT</button>
+
+          <DataTable
+            className="dataTable"
+            value={productData}
+            selectionMode="single"
+            metaKeySelection={false}
+            selection={selectedProduct}
+            onSelectionChange={(e) => setSelectedProduct(e.value)}
+            sortField="prodNick"
+            sortOrder={1}
+            responsiveLayout="scroll"
+            filterDisplay="row"
+            filters={filter}
+          >
+            <Column
+              field="prodName"
+              filterPlaceholder="Search Products"
+              filter
+            />
+          </DataTable>
+          <div className="bottomSpace"></div>
         </React.Fragment>
-      <DataTable
-        className="dataTable"
-        value={productData}
-        selectionMode="single"
-        metaKeySelection={false}
-        selection={selectedProduct}
-        onSelectionChange={(e) => setSelectedProduct(e.value)}
-        sortField="prodNick"
-        sortOrder={1}
-        responsiveLayout="scroll"
-        filterDisplay="row"
-        filters={filter}
-      >
-        <Column field="prodName" filterPlaceholder="Search Products" filter />
-      </DataTable>
-      <div className="bottomSpace"></div>
+      ) : (
+        <React.Fragment>
+          <div className="submitButton">
+            <Button
+              label="Submit"
+              className="p-button-raised p-button-rounded"
+              style={submitButtonStyle}
+              onClick={handleSubmit}
+            />
+          </div>
+          <button onClick={handleClick}>+ PRODUCT LIST</button>
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 }
