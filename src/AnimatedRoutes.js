00f8ae432a4300c26a11f52404b204c80705ad06
Dan Berkeland <danberkeland@gmail.com>
2022-09-17 06:39:06 -0700

    added useLocation to Router

---
+++ src/AnimatedRoutes.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Ordering from "./Pages/Ordering/Ordering";
+import CustomerNews from "./Pages/CustomerNews/CustomerNews";
+import CustomerBilling from "./Pages/CustomerBilling/CustomerBilling";
+import CustomerSettings from "./Pages/CustomerSettings/CustomerSettings";
+import CustomerProducts from "./Pages/CustomerProducts/CustomerProducts";
+import Production from "./Pages/Production/Production"
+import Products from "./Pages/Products/Products"
+import Settings from "./Pages/Settings/Settings"
+import Billing from "./Pages/Billing/Billing"
+import EODCounts from "./Pages/EODCounts/EODCounts"
+import Logistics from "./Pages/Logistics/Logistics"
+import Locations from "./Pages/Locations/Locations";
+import { NavSide } from "./Nav";
+
+function AnimatedRoutes({ Routes, Route, useLocation}) {
+  const location = useLocation();
+
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route path="/Ordering" element={<Ordering />} />
+      <Route path="/CustomerNews" element={<CustomerNews />} />
+      <Route path="/CustomerBilling" element={<CustomerBilling />} />
+      <Route path="/CustomerSettings" element={<CustomerSettings />} />
+      <Route path="/CustomerProducts" element={<CustomerProducts />} />
+      <Route path="/Production" element={<Production />} />
+      <Route path="/Products" element={<Products />} />
+      <Route path="/Settings" element={<Settings />} />
+      <Route path="/Billing" element={<Billing />} />
+      <Route path="/EODCounts" element={<EODCounts />} />
+      <Route path="/Logistics" element={<Logistics />} />
+      <Route path="/Locations" element={<Locations />} />
+      <Route path="/" element={<NavSide />} />
+    </Routes>
+  );
+}
+
+export default AnimatedRoutes;
--- src/Pages.js
+++ src/Pages.js
@@ -1,39 +1,14 @@
 import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
-import Ordering from "./Pages/Ordering/Ordering";
-import CustomerNews from "./Pages/CustomerNews/CustomerNews";
-import CustomerBilling from "./Pages/CustomerBilling/CustomerBilling";
-import CustomerSettings from "./Pages/CustomerSettings/CustomerSettings";
-import CustomerProducts from "./Pages/CustomerProducts/CustomerProducts";
-import Production from "./Pages/Production/Production"
-import Products from "./Pages/Products/Products"
-import Settings from "./Pages/Settings/Settings"
-import Billing from "./Pages/Billing/Billing"
-import EODCounts from "./Pages/EODCounts/EODCounts"
-import Logistics from "./Pages/Logistics/Logistics"
-import Locations from "./Pages/Locations/Locations";
-import { NavSide } from "./Nav";
+
+import AnimatedRoutes from "./AnimatedRoutes";
 
 function Pages() {
   return (
     <Router>
-      <Routes>
-        <Route path="/Ordering" element={<Ordering />} />
-        <Route path="/CustomerNews" element={<CustomerNews />} />
-        <Route path="/CustomerBilling" element={<CustomerBilling />} />
-        <Route path="/CustomerSettings" element={<CustomerSettings />} />
-        <Route path="/CustomerProducts" element={<CustomerProducts />} />
-        <Route path="/Production" element={<Production />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/Settings" element={<Settings />} />
-        <Route path="/Billing" element={<Billing />} />
-        <Route path="/EODCounts" element={<EODCounts />} />
-        <Route path="/Logistics" element={<Logistics />} />
-        <Route path="/Locations" element={<Locations />} />
-        <Route path="/" element={<NavSide />} />
-      </Routes>
+      <AnimatedRoutes Routes={Routes} Route={Route} useLocation={useLocation} />
     </Router>
   );
 }
