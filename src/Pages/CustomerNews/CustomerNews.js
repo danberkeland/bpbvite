0f2e112de2c420d1e28ffe159da8f835736c6c9e
Dan Berkeland <danberkeland@gmail.com>
2022-10-08 15:11:59 -0700

    added photo to home page

--- src/Pages/CustomerNews/CustomerNews.js
+++ src/Pages/CustomerNews/CustomerNews.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "../../index.css";
+import bpblogo from "../../bpblogo.jpg";
+import { motion } from "framer-motion";
 
 function CustomerNews() {
-  return <React.Fragment>Customer News</React.Fragment>;
+  return (
+    <React.Fragment>
+      <motion.div
+        initial={{ opacity: 0, x: "0", y: "0" }}
+        animate={{ opacity: .8, x: "0" }}
+        transition={{ duration: 2.5, ease: "easeInOut" }}
+        exit={{ opacity: 0, x: "0" }}
+        className="photoBox"
+      >
+        <img src={bpblogo} alt="Back Porch Bakery Logo" width="100%" />
+      </motion.div>
+    </React.Fragment>
+  );
 }
 
 export default CustomerNews;
