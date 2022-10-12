6082a7825782e6be9330de6a451fa644de63c8f8
Dan Berkeland <danberkeland@gmail.com>
2022-10-12 12:57:55 -0700

    hoc of bpbform works

--- src/Pages/Products/ProductDetails.js
+++ src/Pages/Products/ProductDetails.js
@@ -1,10 +1,4 @@
-import React, { useEffect } from "react";
-
-import { Form, Formik } from "formik";
-
-import { Button } from "primereact/button";
-import { ConfirmDialog } from "primereact/confirmdialog"; // To use <ConfirmDialog> tag
-import { confirmDialog } from "primereact/confirmdialog"; // To use confirmDialog method // To use confirmDialog method
+import React from "react";
 
 import {
   CustomIDInput,
@@ -17,77 +11,19 @@ import {
 } from "../../FormComponents/CustomInputs";
 import { validationSchema } from "./ValidationSchema";
 
-import styled from "styled-components";
-import { useSettingsStore } from "../../Contexts/SettingsZustand";
 import { deleteProduct, updateProduct, createProduct } from "../../restAPIs";
-import { withFadeIn } from "../../utils";
+import { withFadeIn, withBPBForm } from "../../utils";
+import { GroupBox } from "../../utils";
 
-const GroupBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-content: flex-start;
-  border: 1px solid lightgrey;
-  width: 95%;
-  margin: 5px 10px 20px 10px;
-  padding: 5px 5px 10px 5px;
-`;
+const compose = (...fns) =>
+  fns.reduceRight(
+    (prevFn, nextFn) =>
+      (...args) =>
+        nextFn(prevFn(...args)),
+    (value) => value
+  );
 
-function ProductDetails({ initialState, productList }) {
-  const setIsEdit = useSettingsStore((state) => state.setIsEdit);
-  const isEdit = useSettingsStore((state) => state.isEdit);
-  const isCreate = useSettingsStore((state) => state.isCreate);
-  const setIsCreate = useSettingsStore((state) => state.setIsCreate);
-
-  let str = "product"
-  let source = str+"List"
-  let path = "/"+ str.charAt(0).toUpperCase()+str.slice(1)+"s"
-  let create = "create"+str.charAt(0).toUpperCase()+str.slice(1)
-  console.log("create", create)
-  
-
-  var sourceVar = window[source]
-  let createFunc = window[create]
-  console.log("type",typeof createFunc)
-  
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    setIsEdit(false);
-  }, [setIsEdit]);
-
-  const editButtonStyle = {
-    width: "100px",
-    margin: "20px",
-    fontSize: "1.2em",
-  };
-
-  const handleEdit = (e, props) => {
-    console.log("values", props.values);
-    window.scrollTo(0, 0);
-    setIsEdit(true);
-  };
-
-  const handleDelete = (e, props) => {
-    
-    confirmDialog({
-      message: `Are you sure you want to delete this ${str}?`,
-      header: "Confirmation",
-      icon: "pi pi-exclamation-triangle",
-      accept: () => {
-        console.log("values", props);
-        window.scrollTo(0, 0);
-        setIsEdit(false);
-        setIsCreate(false);
-        deleteProduct(props).then(() => {
-          window.location = path;
-        });
-      },
-      reject: () => {
-        return;
-      },
-    });
-  };
-
+function ProductDetails({ initialState }) {
   const packGroups = [
     { label: "Baked Pastries", value: "baked pastries" },
     { label: "Frozen Pastries", value: "frozen pastries" },
@@ -118,7 +54,10 @@ function ProductDetails({ initialState, productList }) {
     { label: "Carlton", value: "carlton" },
   ];
 
-  const FadeLocationList = withFadeIn((props) => {
+  const BPBLocationForm = compose(
+    withBPBForm,
+    withFadeIn
+  )((props) => {
     return (
       <React.Fragment>
         <GroupBox>
@@ -225,65 +164,14 @@ function ProductDetails({ initialState, productList }) {
   });
 
   return (
-    <div>
-      <ConfirmDialog />
-      <Formik
-        initialValues={initialState}
-        validationSchema={validationSchema(sourceVar)}
-        onSubmit={(props) => {
-          console.log("values", props);
-          window.scrollTo(0, 0);
-          setIsEdit(false);
-          setIsCreate(false);
-          if (isCreate) {
-            createProduct(props).then(() => {
-              window.location = path;
-            });
-          } else {
-            updateProduct(props).then(() => {
-              window.location = path;
-            });
-          }
-        }}
-      >
-        {(props) => (
-          <React.Fragment>
-            <Form>
-              {isEdit | isCreate ? (
-                <div className="floatButtonsTop">
-                  <Button
-                    label="Submit"
-                    type="submit"
-                    className="p-button-raised p-button-rounded p-button-danger"
-                    style={editButtonStyle}
-                  />
-                </div>
-              ) : (
-                <div></div>
-              )}
-              <FadeLocationList {...props} />
-
-              {!isEdit && !isCreate && (
-                <Button
-                  label="Edit"
-                  className="editButton p-button-raised p-button-rounded p-button-success"
-                  style={editButtonStyle}
-                  onClick={(e) => handleEdit(e, props)}
-                />
-              )}
-            </Form>
-            <Button
-              label="Delete"
-              type="delete"
-              className="editButton p-button-raised p-button-rounded p-button-success"
-              style={editButtonStyle}
-              onClick={(e) => handleDelete(e, props)}
-            />
-          </React.Fragment>
-        )}
-      </Formik>
-      <div className="bottomSpace"></div>
-    </div>
+    <BPBLocationForm
+      name="product"
+      validationSchema={validationSchema}
+      initialState={initialState}
+      createProduct={createProduct}
+      deleteProduct={deleteProduct}
+      updateProduct={updateProduct}
+    />
   );
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,14 @@
+import React, { useEffect } from "react";
 
+import { Button } from "primereact/button";
+import { confirmDialog } from "primereact/confirmdialog";
+import { ConfirmDialog } from "primereact/confirmdialog";
+import { Formik, Form } from "formik";
 import { motion } from "framer-motion";
+import { useSettingsStore } from "./Contexts/SettingsZustand";
+import styled from "styled-components";
+
+
 
 export const sortAtoZDataByIndex = (data, index) => {
   try {
@@ -34,3 +43,133 @@ export const withFadeIn = (Component) => (props) => {
     </motion.div>
   );
 };
+
+export const withBPBForm = (Component) => (props) => {
+  const setIsEdit = useSettingsStore((state) => state.setIsEdit);
+  const isEdit = useSettingsStore((state) => state.isEdit);
+  const isCreate = useSettingsStore((state) => state.isCreate);
+  const setIsCreate = useSettingsStore((state) => state.setIsCreate);
+
+  let str = props.name
+  let source = str+"List"
+  let path = "/"+ str.charAt(0).toUpperCase()+str.slice(1)+"s"
+  let create = "create"+str.charAt(0).toUpperCase()+str.slice(1)
+  console.log("create", create)
+  let fns = props
+
+  var sourceVar = window[source]
+  let createFunc = window[create]
+  console.log("type",typeof createFunc)
+  
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setIsEdit(false);
+  }, [setIsEdit]);
+
+  const editButtonStyle = {
+    width: "100px",
+    margin: "20px",
+    fontSize: "1.2em",
+  };
+
+  const handleEdit = (e, props) => {
+    console.log("values", props.values);
+    window.scrollTo(0, 0);
+    setIsEdit(true);
+  };
+
+  const handleDelete = (e, props) => {
+    
+    confirmDialog({
+      message: `Are you sure you want to delete this ${str}?`,
+      header: "Confirmation",
+      icon: "pi pi-exclamation-triangle",
+      accept: () => {
+        console.log("values", props);
+        window.scrollTo(0, 0);
+        setIsEdit(false);
+        setIsCreate(false);
+        fns.deleteProduct(props).then(() => {
+          window.location = path;
+        });
+      },
+      reject: () => {
+        return;
+      },
+    });
+  };
+
+  return (
+    <div>
+      <ConfirmDialog />
+      <Formik
+        initialValues={props.initialState}
+        validationSchema={props.validationSchema(sourceVar)}
+        onSubmit={(props) => {
+          console.log("values", props);
+          window.scrollTo(0, 0);
+          setIsEdit(false);
+          setIsCreate(false);
+          if (isCreate) {
+            fns.createProduct(props).then(() => {
+              window.location = path;
+            });
+          } else {
+            fns.updateProduct(props).then(() => {
+              window.location = path;
+            });
+          }
+        }}
+      >
+        {(props) => (
+          <React.Fragment>
+            <Form>
+              {isEdit | isCreate ? (
+                <div className="floatButtonsTop">
+                  <Button
+                    label="Submit"
+                    type="submit"
+                    className="p-button-raised p-button-rounded p-button-danger"
+                    style={editButtonStyle}
+                  />
+                </div>
+              ) : (
+                <div></div>
+              )}
+              <Component {...props} />
+
+              {!isEdit && !isCreate && (
+                <Button
+                  label="Edit"
+                  className="editButton p-button-raised p-button-rounded p-button-success"
+                  style={editButtonStyle}
+                  onClick={(e) => handleEdit(e, props)}
+                />
+              )}
+            </Form>
+            <Button
+              label="Delete"
+              type="delete"
+              className="editButton p-button-raised p-button-rounded p-button-success"
+              style={editButtonStyle}
+              onClick={(e) => handleDelete(e, props)}
+            />
+          </React.Fragment>
+        )}
+      </Formik>
+      <div className="bottomSpace"></div>
+    </div>
+  );
+};
+
+
+export const GroupBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  border: 1px solid lightgrey;
+  width: 95%;
+  margin: 5px 10px 20px 10px;
+  padding: 5px 5px 10px 5px;
+`;
