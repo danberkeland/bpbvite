f1248d92325ed98cf39922e8c21e5034f85041e6
dkatosan <dkatosanma@gmail.com>
2022-11-14 13:06:31 -0800

    Ordering > edit qty and update totals

--- src/Pages/Ordering/Orders2.jsx
+++ src/Pages/Ordering/Orders2.jsx
@@ -9,6 +9,7 @@ import * as mockData from './mockData'
 import { Dropdown } from "primereact/dropdown"
 import { Button } from "primereact/button"
 import { TabMenu } from "primereact/tabmenu"
+import { Badge } from "primereact/badge"
 
 import './orders.css'
 import { DataTable } from "primereact/datatable"
@@ -44,21 +45,14 @@ const Orders2 = () => {
   // order list area
   const [activeIndex, setActiveIndex] = useState(0)
   const [orderTable, setOrderTable] = useState()
-  const [orderTableRender, setOrderTableRender] = useState()
   
   // Probably has bugs -- convert to Luxon later!
   useEffect(() => {
     let date = new Date()
     date.setDate(date.getDate() + 2)
     setSelectedDate(date)
+    setSelectedWeekday(weekdays[((date).getDay())])
   }, []);
-
-  useEffect( () => {
-    if (orderTable) {
-      const filteredOrderTable = orderTable.filter(item => item.route === tabIndexFulfillmentTypes[activeIndex])
-      setOrderTableRender(filteredOrderTable)
-    }
-  }, [activeIndex])
   
   return (
       <div className="pageContainer">
@@ -116,46 +110,79 @@ const Orders2 = () => {
                     ...item, total: (item.qty * item.rate)
                   }))
                   setOrderTable(prodsWithTotals)
-                  setOrderTableRender(prodsWithTotals.filter(item => item.route === tabIndexFulfillmentTypes[activeIndex]))
                 }}
                 disabled={!selectedDate || !selectedLocation}
               />
             </div>
           </div>
-        
         </div>
         
         {/* Order List Area */}
 
         <div className="card">
           <header className="card-header">
-            <h3>Order List</h3>
+            <h2>Order List</h2>
           </header>
 
-          <div className="card-body">
+
             <TabMenu 
               model={[
-                {label: 'Delivery', icon: 'pi pi-send'},
-                {label: 'Pickup SLO', icon: 'pi pi-shopping-bag'},
-                {label: 'Pickup Carlton', icon: 'pi pi-shopping-bag'},
+                {label: `Delivery${orderTable ? ' (' + orderTable.filter(item => item.route === 'deliv').length + ')' : ''}`, icon: 'pi pi-send'},
+                {label: `Pickup SLO${orderTable ? ' (' + orderTable.filter(item => item.route === 'slopick').length + ')' : ''}`, icon: 'pi pi-shopping-bag'},
+                {label: `Pickup Carlton${orderTable ? ' (' + orderTable.filter(item => item.route === 'atownpick').length + ')' : ''}`, icon: 'pi pi-shopping-bag'},
               ]} 
               activeIndex={activeIndex}
               onTabChange={e => setActiveIndex(e.index)}
             />
-
+            
             <DataTable 
-              value={orderTableRender}
+              className="editable-cells-table"
+              value={orderTable && orderTable.filter(item => (item.route === tabIndexFulfillmentTypes[activeIndex]))}
               responsiveLayout="scroll"
               editMode="cell"
+              footer={
+                <>
+                  <div style={{display: "inline-block", width: "30%"}}>
+                    <Button label="Add +" 
+                      className="inline"
+                      onClick={() => {
+                      
+                      }}
+                    /> 
+                  </div>
+                  <div style={{display: "inline-block", width: "70%", height: "100%", textAlign: "center", verticalAlign: "middle"}}>
+                    {orderTable && `Grand Total: ${dollarUS.format(orderTable.reduce((total, current) => {
+                      return (total + current.total)
+                    }, 0))}`}
+                  </div>
+                </>
+              }
               // className="editable-cells-table"
             >
               <Column field="prodNick" header="Product" />
               <Column field="rate" header="Rate" />
               <Column field="qty" 
                 header="Quantity"
+                style={{ width: '20%' }}
                 editor={(options) => { return(
-                  <InputNumber value={options.value} onValueChange={(e) => options.editorCallback(e.value)} />
+                    <InputNumber 
+                      className="p-fluid" 
+                      value={options.value} 
+                      onValueChange={(e) => options.editorCallback(e.target.value)} 
+                    />
                 )}}
+                onCellEditComplete={e => {
+                  let { rowData, newValue, field, originalEvent: event } = e
+                  if (newValue >= 0) {
+                    let newTableData = orderTable.slice()
+                    newTableData.forEach( (item, idx) => {
+                      if (item.id === rowData.id) {
+                        newTableData[idx] = {...newTableData[idx], qty: newValue, total: (rowData.rate * newValue)}
+                      }})
+                    setOrderTable(newTableData)
+                  } else
+                    event.preventDefault()
+                }}
               />
               <Column 
                 field="total" 
@@ -165,6 +192,9 @@ const Orders2 = () => {
               
             </DataTable>
           </div>
+          <div className="card-footer" style={{backgroundColor:"lightGrey"}}>
+
+
         </div>
         
         {/* Variables */}
@@ -172,6 +202,7 @@ const Orders2 = () => {
           <header className="card-header">
             <h3>Variables</h3>
           </header>
+
           <div className="card-body">
             <pre>selectedDate: {JSON.stringify(selectedDate, null, 2)}</pre>
             <pre>selectedWeekday: {JSON.stringify(selectedWeekday, null, 2)}</pre>
@@ -180,6 +211,7 @@ const Orders2 = () => {
             <pre>activeIndex: {JSON.stringify(activeIndex, null, 2)}</pre>
           </div>
         </div>
+        <div style={{height: "125px"}} />
       </div>
 
 
