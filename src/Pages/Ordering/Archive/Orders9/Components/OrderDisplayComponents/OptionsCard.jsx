8fd145c8b7fc2cb9c71476c5bbc85d473bab72ba
Dan Berkeland <danberkeland@gmail.com>
2023-02-04 09:28:39 -0800

    converted radio buttons to dropdown on fulfill options

--- src/Pages/Ordering/Archive/Orders9/Components/OrderDisplayComponents/OptionsCard.jsx
+++ src/Pages/Ordering/Archive/Orders9/Components/OrderDisplayComponents/OptionsCard.jsx
@@ -1,84 +1,86 @@
-import React from "react"
+import React from "react";
 
-import { Card } from "primereact/card"
-import { RadioButton } from "primereact/radiobutton"
-import { InputTextarea } from "primereact/inputtextarea"
+import { Card } from "primereact/card";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Dropdown } from "primereact/dropdown";
 
 /**For display/control of header-type order info*/
 export const OptionsCard = ({ orderHeaderState, readOnly }) => {
-  const { orderHeader, orderHeaderChanges, setOrderHeaderChanges} = orderHeaderState
+  const { orderHeader, orderHeaderChanges, setOrderHeaderChanges } =
+    orderHeaderState;
+
+  const fulfillOptions = [
+    {
+      label:
+        orderHeader?.defaultRoute === "deliv"
+          ? "Delivery (default)"
+          : "Delivery",
+      value: "deliv",
+    },
+    {
+      label:
+        orderHeader?.defaultRoute === "slopick"
+          ? "Pick up SLO (default)"
+          : "Pick up SLO",
+      value: "slopick",
+    },
+    {
+      label:
+        orderHeader?.defaultRoute === "atownpick"
+          ? "Pickup Carlton (default)"
+          : "Pickup Carlton",
+      value: "atownpick",
+    },
+  ];
 
   return (
-    <Card 
-      style={{marginTop: "0px"}}
+    <Card
+      style={{ marginTop: "0px" }}
       //title={readOnly ? "Items (Read Only)" : "Items"}
     >
-      {orderHeaderChanges &&
-        <div style={{marginBottom: "30px"}}>
-          <CustomRadioButton label='Delivery' value='deliv'
-            orderHeaderState={orderHeaderState}
-            hidden={orderHeader.defaultRoute !== 'deliv'}
-            disabled={readOnly}
-          />
-
-          <CustomRadioButton label='Pick up SLO' value='slopick'
-            orderHeaderState={orderHeaderState}
-            disabled={readOnly}
-          />
-
-          <CustomRadioButton label='Pick up Carlton' value='atownpick'
-            orderHeaderState={orderHeaderState}
-            disabled={readOnly}
+      {orderHeaderChanges && (
+        <div style={{ marginBottom: "30px" }}>
+          <Dropdown
+            id="filfillDropdown"
+            options={fulfillOptions}
+            optionLabel="label"
+            optionValue="value"
+            value={orderHeaderChanges.route}
+            onChange={(e) =>
+              setOrderHeaderChanges({ ...orderHeaderChanges, route: e.value })
+            }
           />
         </div>
-      }
+      )}
 
-      {orderHeaderChanges &&
+      {orderHeaderChanges && (
         <span className="p-float-label">
           <InputTextarea
             id="input-note"
-            style={{width: "100%"}}
+            style={{ width: "100%" }}
             value={orderHeader.ItemNote ? orderHeader.itemNote : undefined}
-            onChange={e => setOrderHeaderChanges({ ...orderHeader, ItemNote: e.target.value ? e.target.value : null })}
+            onChange={(e) =>
+              setOrderHeaderChanges({
+                ...orderHeader,
+                ItemNote: e.target.value ? e.target.value : null,
+              })
+            }
             disabled={readOnly}
           />
-          <label htmlFor="input-note"
-            style={{fontWeight: orderHeader.ItemNote !== orderHeaderChanges.ItemNote ? "bold" : "normal"}}
+          <label
+            htmlFor="input-note"
+            style={{
+              fontWeight:
+                orderHeader.ItemNote !== orderHeaderChanges.ItemNote
+                  ? "bold"
+                  : "normal",
+            }}
           >
-            {"Add a Note" + (orderHeader.ItemNote !== orderHeaderChanges.ItemNote ? "*" : '')}
+            {"Add a Note" +
+              (orderHeader.ItemNote !== orderHeaderChanges.ItemNote ? "*" : "")}
           </label>
         </span>
-      }
-
+      )}
     </Card>
-  )
-}
-
-
-
-const CustomRadioButton = ({value, label, orderHeaderState, hidden, disabled}) => {
-  const { orderHeader, orderHeaderChanges, setOrderHeaderChanges } = orderHeaderState
-  
-  const isDefaultOption = value === orderHeader.defaultRoute
-  const checked = value === orderHeaderChanges.route
-  const valueUpdated = orderHeader.route !== orderHeaderChanges.route
-
-  return (
-    <div style={{margin: "5px"}}>
-      <RadioButton 
-        inputId={value} 
-        value={value} 
-        checked={checked}
-        onChange={e => setOrderHeaderChanges({...orderHeaderChanges, route: e.value})}
-        hidden={hidden || false}
-        disabled={disabled}
-      />
-      <label 
-        htmlFor={value} 
-        style={{fontWeight: checked && valueUpdated ? "bold" : "normal"}}
-      >
-        {isDefaultOption ? label + " (default)" : label}
-      </label>
-    </div>
-  )
-}
+  );
+};
