4eb3b329c2aae20aee218e74a38a590a38269937
Dan Berkeland <danberkeland@gmail.com>
2023-02-15 16:58:56 -0800

    had to cover up tableData error in StandingOrder.jsx Orders10

--- src/Pages/Ordering/Orders10/StandingOrder/StandingOrder.jsx
+++ src/Pages/Ordering/Orders10/StandingOrder/StandingOrder.jsx
@@ -320,6 +320,9 @@ const makeStandingBase = (standingData, isStand, isWhole) => {
   const _standingData = standingData
     .filter(item => item.isStand === isStand && item.isWhole === isWhole)
 
+
+  /*
+
   const weekdays = weekdayOptions.map(i => i.value)
   const products = [...new Set (tableData.map(i => i.product.prodNick))]
 
@@ -336,6 +339,6 @@ const makeStandingBase = (standingData, isStand, isWhole) => {
   }
   
   
-
+*/
 
 }
