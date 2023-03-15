7508a8b26bd68886baedecb86e400ba359f36e2d
dkatosan <dkatosanma@gmail.com>
2023-03-15 14:59:42 -0700

    map data to legacy format; add legacy functions

---
+++ src/customGraphQL/queries/legacyQueries.js
@@ -0,0 +1,255 @@
+const locationQuery = /* GraphQL */ `
+  listLocations(
+    # locNick: $locNick
+    # filter: $filter
+    limit: $limit
+    # nextToken: $nextToken
+    # sortDirection: $sortDirection
+  ) {
+    items {
+      # Type
+      locNick
+      locName
+      subs {
+        items {
+          sub
+        }
+      }
+      # standing {}
+      # orders {}
+      # ordersByDate {}
+      zoneNick
+      # zone {}
+      # creditApp {}
+      addr1
+      addr2
+      city
+      zip
+      email
+      phone
+      firstName
+      lastName
+      toBePrinted
+      toBeEmailed
+      printDuplicate
+      terms
+      invoicing
+      latestFirstDeliv
+      latestFinalDeliv
+      webpageURL
+      picURL
+      gMap
+      specialInstructions
+      delivOrder
+      qbID
+      currentBalance
+      # isActive
+      # prodsNotAllowed {} # maybe use?
+      # customProd {} # maybe use?
+      # templateProd {} # maybe use?
+      # altLeadTimeByProduct {}
+      # createdAt
+      # updatedAt
+      # locationCreditAppId
+    }
+    # nextToken
+  }
+`;
+const productQuery = /* GraphQL */ `
+  listProducts(
+    # prodNick: $prodNick
+    # filter: $filter
+    limit: $limit
+    # nextToken: $nextToken
+    # sortDirection: $sortDirection
+  ) {
+    items {
+      # Type
+      prodName
+      prodNick
+      packGroup
+      packSize
+      doughNick
+      # doughType {
+      #   doughNick
+      #   doughName
+      #   hydration
+      #   batchSize
+      #   mixedWhere
+      #   isBakeReady
+      #   buffer
+      #   saltInDry
+      #   createdAt
+      #   updatedAt
+      # }
+      freezerThaw
+      packGroupOrder
+      # shapeDay
+      # shapeNick
+      # bakeDay
+      # bakeNick
+      # guarantee
+      # transferStage
+      readyTime
+      bakedWhere
+      wholePrice
+      retailPrice
+      # isRetail
+      # retailName
+      # retailDescrip
+      isWhole
+      isEOD
+      weight
+      descrip
+      picURL
+      squareID
+      forBake
+      bakeExtra
+      batchSize
+      defaultInclude
+      leadTime
+      qbID
+      currentStock
+      freezerClosing
+      freezerCount
+      freezerNorth
+      freezerNorthClosing
+      freezerNorthFlag
+      prepreshaped
+      preshaped
+      updatePreDate
+      # retailLoc {}
+      # standing {}
+      # orders {}
+      # depends {
+      #   prod1Nick
+      #   prod1 {
+      #     prodName
+      #   }
+      #   prod2Nick
+      #   prod2 {
+      #     prodName
+      #   }
+      # }
+      # altPricing {}
+      # templateProd {}
+      # prodsNotAllowed {}
+      # productVendor {}
+      EODCount {
+        prodNick
+        shelfOrFreezer
+        startOrFinish
+        location
+        qty
+        whoCounted
+        # createdAt
+        # updatedAt
+      }
+      ActualSetOut {
+        prodNick
+        qty
+        location
+        whoSetOut
+        # createdAt
+        # updatedAt
+      }
+      # altLeadTimeByLocation {}
+      # createdAt
+      # updatedAt
+      # inventoryProductId # not in schema???
+    }
+    nextToken
+  }
+`;
+const routeQuery = /* GraphQL */ `
+  listRoutes(
+    limit: $limit
+  ) {
+    items {
+      routeNick
+      routeName
+      routeStart
+      routeTime
+      RouteDepart
+      RouteArrive
+      RouteSched
+      printOrder
+      driver
+      zoneRoute {
+        items {
+          zoneNick
+        }
+      }
+    }
+  }
+`;
+export const orderQuery = /* GraphQL */ `
+  listOrders(limit: $limit) {
+    items {
+      # Type
+      id
+      qty
+      # qtyUpdatedOn
+      # sameDayMaxQty
+      # prodNick
+      product {
+        prodName
+      }
+      # locNick
+      location {
+        locName
+      }
+      ItemNote
+      SO
+      isWhole
+      delivDate
+      rate
+      route
+      isLate
+      # createdOn
+      updatedOn
+      # updatedBy
+      # ttl
+    }
+    nextToken
+  }
+`;
+export const standingQuery = /* GraphQL */ `
+    listStandings(limit: $limit) {
+      items {
+        # id
+        # locNick
+        location {
+          locName
+        }
+        isWhole
+        isStand
+        dayOfWeek
+        route
+        # prodNick
+        product {
+          prodName
+        }
+        qty
+        # ItemNote
+        # startDate
+        # endDate
+        # createdAt
+        updatedAt
+        # updatedBy
+      }
+      nextToken
+    }
+`;
+
+export const getLegacyDatabase = /* GraphQL */ `
+  query GetLegacyDatabase(
+    $limit: Int
+  ) {
+    ${locationQuery}
+    ${productQuery}
+    ${routeQuery}
+    ${orderQuery}
+    ${standingQuery}
+  }
+`;
---
+++ src/data/legacyData.js
@@ -0,0 +1,199 @@
+import { useMemo } from "react"
+import useSWR, { mutate } from "swr"
+import { defaultSwrOptions } from "./constants"
+
+import gqlFetcher from "./fetchers"
+import * as legacyQueries from "../customGraphQL/queries/legacyQueries"
+
+
+/**
+ * Fetches data from the current database and transforms it to
+ * Legacy format. The intent is to make the translation here so
+ * that legacy functions that build production reports, etc. can
+ * be plugged in without any alteration.
+ * 
+ * Those functions are housed in legacyDataFunctions.js
+ */
+export const useLegacyFormatDatabase = () => {
+
+  const query = legacyQueries.getLegacyDatabase
+  const variables = {
+    limit: 2000
+  }
+
+  const { data, errors } = useSWR(
+    [query, variables], 
+    gqlFetcher, 
+    defaultSwrOptions
+  )
+
+  const transformData = () => {
+    if (!data) return undefined
+
+    const products = mapProductsToLegacy(data.data.listProducts.items)
+    const customers = mapLocationsToLegacy(data.data.listLocations.items)
+    const routes = mapRoutesToLegacy(data.data.listRoutes.items)
+    const standing = mapStandingItemsToLegacy(data.data.listStandings.items)
+    const orders = mapOrdersToLegacy(data.data.listOrders.items)
+
+    return ([
+      products,
+      customers,
+      routes,
+      standing,
+      orders
+    ])
+  }
+
+  const _data = useMemo(transformData, [data])
+
+  return ({
+    data: _data,
+    errors: errors
+  })
+
+
+}
+
+
+
+// *****MAPPING FUNCTIONS******
+//
+// Same strategy for most functions:
+// For each item, we pick out attributes whose key name
+// or value format changes. We also pick out
+// attributes that for some reason were fetched, but
+// shouldn't be included in the final item/object.
+// Attributes that require no manipulation are bundled
+// as "...unchangedAttributes".
+//
+// We return an object with the correct key names
+// and/or adjusted value formats.
+
+const mapLocationsToLegacy = (locations) => locations.map(location => {
+  const { locNick, locName, subs, zoneNick, ...unchangedAttributes } = location
+  
+  return ({
+    ...unchangedAttributes,
+    nickName: locNick,
+    custName: locName,
+    userSubs: subs.items.map(s => s.sub),
+    zoneName: zoneNick
+  })
+})
+
+
+const mapProductsToLegacy = (products) => products.map(product => {
+  const { prodNick, doughNick, isEOD, EODCount, ActualSetOut, ...unchangedAttributes } = product
+  
+  return ({
+    ...unchangedAttributes,
+    nickName: prodNick,
+    doughType: doughNick,
+    eodCount: isEOD,
+    whoCountedLast: EODCount?.whoCounted || null
+  })
+})
+
+
+const mapRoutesToLegacy = (routes) => routes.map(route => {
+  const { routeNick, zoneRoute, ...unchangedAttributes } = route
+  
+  return ({
+    ...unchangedAttributes,
+    routeName: routeNick,
+    RouteServe: zoneRoute.items.map(zr => zr.zoneNick)
+  })
+  
+})
+
+
+
+const yyyymmddToMmddyyyy = (yyyymmddDateString) => {
+  const dateParts = yyyymmddDateString.split('-')
+  return `${dateParts[1]}/${dateParts[2]}/${dateParts[0]}`
+}
+
+const mapOrdersToLegacy = (orders) => orders.map(order => {
+  const { product, location, ItemNote, delivDate, updatedOn, ...unchangedAttributes } = order
+
+  return ({
+    ...unchangedAttributes,
+    prodName: product.prodName,
+    custName: location.locName,
+    SO: order.qty,
+    delivDate: yyyymmddToMmddyyyy(delivDate),
+    PONote: ItemNote,
+    timestamp: updatedOn
+  })
+
+})
+
+// legacy standing Attributes:
+// prodName: String
+// custName: String
+// isStand: boolean
+// Sun, ..., Sat: Int
+
+// This one is going be more involved, because new standing items only handle
+// one day of the week at a time. So we need to combine up to 7 records to 
+// recover a legacy standing item.
+
+// we should assume new records are unique combinations of
+// locName, prodName, isStand, and dayOfWeek.
+// We will key transformed (legacy format) records on the first three,
+// and detect possible duplicates when a value is written to the same
+// weekday qty attribute more than once.
+
+const mapStandingItemsToLegacy = (standingItems) => {
+  
+  let init = {
+    items: {},
+    duplicates: []
+  }
+  let _standingItems = standingItems
+    .filter(item => item.isWhole)
+    .reduce((prev, curr) => {
+      let next = {...prev}
+      let locName = curr.location.locName
+      let prodName = curr.product.prodName
+      let isStand = curr.isStand
+      let dataKey = `${locName}#${prodName}#${isStand}#`
+      if (dataKey in prev.items) {
+        if (prev.items[dataKey][curr.dayOfWeek] !== null) next.duplicates = prev.duplicates.concat(curr)
+        else next.items[dataKey][curr.dayOfWeek] = curr.qty
+      } else {
+        next.items[dataKey] = {
+          timestamp: curr.updatedAt,
+          prodName: prodName,
+          custName: locName,
+          isStand: isStand,
+          Sun: null,
+          Mon: null,
+          Tue: null,
+          Wed: null,
+          Thu: null,
+          Fri: null,
+          Sat: null,
+        }
+        next.items[dataKey][curr.dayOfWeek] = curr.qty
+      }
+      return next
+    }, init)
+
+  if (_standingItems.duplicates.length) {
+    console.log("Warning, duplicate standing items found:")
+    console.log(_standingItems.duplicates)
+  }
+
+  // set any empty quantities to 0
+  let returnItems = Object.values(_standingItems.items).map(item => {
+    for (let day of ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]) {
+      if (item[day] === null) item[day] = 0
+    }
+    return item
+  })
+
+  return returnItems
+
+}
