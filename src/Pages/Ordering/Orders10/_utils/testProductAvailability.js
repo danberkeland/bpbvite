d5934b8b7d8ef3a9cb74bec49412bd7454667823
dkatosan <dkatosanma@gmail.com>
2023-03-22 20:59:45 -0700

    factoring out reused code

---
+++ src/Pages/Ordering/Orders10/_utils/testProductAvailability.js
@@ -0,0 +1,7 @@
+// until Product attributes are entered, we use this function to determine whether
+// or not to allow ordering products depending on the day of the week.
+
+export const testProductAvailability = (prodNick, dayOfWeek) => {
+  if (['ptz', 'pzb', 'pzsl', 'pzst', 'unpz'].includes(prodNick) && ['Sun', 'Mon'].includes(dayOfWeek)) return false
+  return true  
+}
