db1f16bd3f385d16ba514b6c2d6229527260d425
dkatosan <dkatosanma@gmail.com>
2023-04-06 17:06:56 -0700

    change Production.js to directory for new pages

--- src/Pages/Production/Production.js
+++ src/Pages/Production/Production.js
@@ -1,114 +1,59 @@
-import React, { useState, useEffect } from "react";
-import { useLogisticsDataByDate, useRouteGrid } from "../../data/productionData";
+import React, { useState } from "react";
 
-import { Calendar } from "primereact/calendar"
-import { Column } from "primereact/column"
-import { DataTable } from "primereact/datatable"
-import { Dropdown } from "primereact/dropdown"
+import { BPBNBaker1 } from "./NewPages/BPBNBaker1New";
+import { BPBNBaker2 } from "./NewPages/BPBNBaker2New";
+import { BPBNSetout } from "./NewPages/BPBNSetoutNew";
+import { WhoBake } from "./NewPages/WhoBakeNew";
+import { WhoShape } from "./NewPages/WhoShapeNew";
 
-import { dateToYyyymmdd } from "../../functions/dateAndTime";
-import dynamicSort from "../../functions/dynamicSort";
+const bpbnPages = [
+  {name: "BPBNBaker1", component: <BPBNBaker1 />},
+  {name: "BPBNBaker2", component: <BPBNBaker2 />},
+  {name: "BPBNSetout", component: <BPBNSetout />},
+  {name: "WhoBake", component: <WhoBake />},
+  {name: "WhoShape", component: <WhoShape />},
+]
 
+const Production = () => {
+  const [pageIndex, setPageIndex] = useState(null)
 
-function Production() {
-  const [calendarDate, setCalendarDate] = useState(new Date())
-  const delivDate = dateToYyyymmdd(calendarDate)
+  return(
+    <div className="production-page-container"
+      style={{
+        maxWidth: "50rem",
+        margin: "auto",
+        marginBottom: "5rem"
+      }}
+    >
+      <h1>BPBN Pages</h1>
+      {bpbnPages.map((item, idx) => <button key={idx}
+          style={{
+            display: "block",
+            width: "10rem",
+            margin: ".5rem 0 .5rem 0",
+            borderRadius: "3px",
+            backgroundColor: idx === pageIndex ? "hsl(240, 18%, 34%)" : "",
+          }} 
+          onClick={() => setPageIndex(idx)} 
+        >
+          {item.name}
+        </button>
+      )}
 
-  const { dimensionData, routedOrderData } = useLogisticsDataByDate(calendarDate, !!calendarDate)
-
-  const routeOptions = routedOrderData 
-    ? [...new Set(routedOrderData.map(o => o.routeNick))]
-      .map(r => ({routeNick: r}))
-      .sort(dynamicSort("routeNick"))
-    : []
-  const [selectedRoute, setSelectedRoute] = useState("AM Pastry")
-
-  const tableData = (routedOrderData && selectedRoute)
-    ? makeGrid(routedOrderData, selectedRoute, dimensionData.locations)
-    : null
-
-  console.log(tableData);
-  return (
-    <div style={{ padding: "1rem", marginBottom: "6rem" }}>
-      <Calendar
-        touchUI={true}
-        value={calendarDate}
-        onChange={(e) => setCalendarDate(e.value)}
-        readOnlyInput // prevent keyboard input of invalid date string
-      />
-      <Dropdown 
-        options={routeOptions}
-        optionLabel="routeNick"
-        optionValue="routeNick"
-        value={selectedRoute}
-        onChange={(e) => setSelectedRoute(e.value)}
-      />
-      <DataTable
-        value={tableData || []}
-        responsiveLayout
-        showGridlines
-        size="small"
+      <div className="production-report-container" 
+        style={{
+          margin: "auto",
+          padding: "2rem",
+          maxWidth: "40rem",
+          //border: "solid 1px black"
+        }}
       >
-        <Column
-          style={{ width: "200px" }}
-          header="Location"
-          field="location.locName"
-        />
-        {!!tableData &&
-          Object.keys(tableData[0].productQtys).map((prodNick) => {
-            return (
-              <Column
-                // headerStyle={{transform: "rotate(300deg)"}}
-                bodyStyle={{ textAlign: "center", width: "3rem" }}
-                header={prodNick}
-                key={prodNick}
-                field={`productQtys.${prodNick}`}
-              />
-            );
-          })}
-      </DataTable>
+        {pageIndex !== null && bpbnPages[pageIndex].component}
+      </div>
     </div>
-  );
-
-  // }
+  )
+  
+  
 }
 
 export default Production;
-
-const makeGrid = (orders, selectedRoute, locations) => {
-  orders = orders.filter(item => item.routeNick === selectedRoute)
-
-  const productList = 
-    // [...new Set(orders.map(order => order.product.prodNick))]
-    [...new Set(orders.map(order => order.prodNick))]
-
-  const locationList = 
-    // [...new Set(orders.map(order => order.location.locNick))]
-    [...new Set(orders.map(order => order.locNick))]
-
-  console.log(locationList, productList);
-
-  let gridData = [];
-  for (let locNick of locationList) {
-    // let ordersByLocation = orders.filter(item => item.location.locNick === locNick)
-    let ordersByLocation = orders.filter(item => item.locNick === locNick)
-
-    let productQtys = Object.fromEntries(
-      // productList.map(prodNick => ([prodNick, ordersByLocation.find(order => order.product.prodNick === prodNick)?.qty || ""]))
-      productList.map(prodNick => ([prodNick, ordersByLocation.find(order => order.prodNick === prodNick)?.qty || ""]))
-    )
-
-    let rowData = {
-      location: locations[locNick],
-      productQtys: productQtys
-    }
-    gridData.push(rowData)
-  
-  }
-
-  gridData.sort((a, b) => {
-    return a.location.delivOrder - b.location.delivOrder;
-  });
-
-  return gridData;
-};
