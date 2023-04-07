912502cf65ccf663e92e5b332f529cb445334311
dkatosan <dkatosanma@gmail.com>
2023-04-06 17:07:25 -0700

    adding fns

--- src/functions/detectDuplicates.js
+++ src/functions/detectDuplicates.js
@@ -1,34 +1,6 @@
-// General-purpose function "bucketing" data
-// by matching values across one or more
-// attrubutes.
-// 
-// Returns an object keyed by observed 
-// combinations of attribute values, delimited 
-// with the '#' character. Values are 'bucket'
-// arrays containing all items matching the key
-// values.
-//
-// Supports specifying nested properties with
-// lodash-like "." separators.
-
 import getNestedObject from "./getNestedObject"
+import { groupBy } from "./groupBy"
 
-const groupBy = (objectArray, keyAtts) => {
-
-  return objectArray.reduce((acc, obj) => {
-    const key = keyAtts
-      .map(attName => {
-        let path = attName.split('.')
-        return getNestedObject(obj, path)
-      })
-      .join('#')
-    
-    const curGroup = acc[key] ?? []
-
-    return { ...acc, [key]: [...curGroup, obj] }
-  }, {})
-  
-}
 
 // items with matching values along key attributes are returned
 // in array buckets. if objectArray has no duplicates, returns
