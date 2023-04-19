c3bedf4907c6e0afa3b07be98abf058e4adb9a2c
Dan Berkeland <danberkeland@gmail.com>
2023-04-19 11:37:46 -0700

    working on notes

--- src/Pages/Settings/notes/Notes2.js
+++ src/Pages/Settings/notes/Notes2.js
@@ -4,17 +4,27 @@ import { useState } from "react";
 import { getWorkingDateTime } from "../../../functions/dateAndTime";
 import Messages from "./Parts2/Messages";
 
-function Notes2() {
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./Notes.css";
 
-    const [delivDate, setDelivDate] = useState(
-        new Date(getWorkingDateTime("NOW").plus({ days: 1 }).toISO())
-      )
+function Notes2() {
+  const [delivDate, setDelivDate] = useState(
+    new Date(getWorkingDateTime("NOW").plus({ days: 1 }).toISO())
+  );
 
   return (
-    <div>
-      <h1>Long Driver Notes</h1>
-      <NotesCalendar delivDate={delivDate} setDelivDate={setDelivDate} />
-      <Messages />
+    <div className="long-driver-notes-container">
+      <h1 className="title">Long Driver Notes</h1>
+      <div className="calendar-datatable-container">
+        <div className="calendar-container">
+          <NotesCalendar delivDate={delivDate} setDelivDate={setDelivDate} />
+        </div>
+        <div className="datatable-container">
+          <Messages />
+        </div>
+      </div>
     </div>
   );
 }
