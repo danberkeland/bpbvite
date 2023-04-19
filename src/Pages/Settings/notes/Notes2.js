62114c5e784941c39488e790dbd43b33f988e211
Dan Berkeland <danberkeland@gmail.com>
2023-04-19 16:50:31 -0700

    focused css on notes

--- src/Pages/Settings/notes/Notes2.js
+++ src/Pages/Settings/notes/Notes2.js
@@ -7,7 +7,7 @@ import Messages from "./Parts2/Messages";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import "./Notes.css";
+import './Notes.css'
 import { useNotesList } from "../../../data/notesData";
 
 function Notes2() {
@@ -19,7 +19,7 @@ function Notes2() {
   console.log('notes', notes)
 
   return (
-    <div className="long-driver-notes-container">
+    <div className="notes long-driver-notes-container">
       <h1 className="title">Long Driver Notes</h1>
       <div className="calendar-datatable-container">
         <div className="calendar-container">
