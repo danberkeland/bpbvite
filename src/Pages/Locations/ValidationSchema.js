1cb74830ca32e5fe5eda27ae4dd15ec760dfc3ac
dkatosan <dkatosanma@gmail.com>
2023-04-24 10:27:03 -0700

    mock up location yup schema

--- src/Pages/Locations/ValidationSchema.js
+++ src/Pages/Locations/ValidationSchema.js
@@ -37,3 +37,71 @@ export const validationSchema = (locationList) => {
 
   });
 };
+
+// const buildLocationSchema = (locNicks, locNames, zoneNicks) => {
+//   const contextSchema = yup.object({
+//     action: yup.string().oneOf(["create", "update"])
+//   })
+
+//   const nameSchema = yup.object({
+//     locNick: yup.string().required().oneOf(locNicks)
+//       .meta({isPK: true }),
+//     locName: yup.string().required()
+
+//   }).meta({ group: "name" })
+
+//   const contactSchema = yup.object({
+//     firstName: yup.string(),
+//     lastName: yup.string(),
+//     phone: yup.string(),
+//     email: yup.string().email(),
+//   }).meta({ group: "contact"})
+  
+//   const billingSchema = yup.object({
+//     qbID: yup.string(),
+//     toBePrinted: yup.boolean(),
+//     toBeEmailed: yup.boolean(),
+//     printDuplicate: yup.boolean(),
+//     terms: yup.string().oneOf(["0", "15", "30"]),
+//     invoicing: yup.string().oneOf(["daily", "weekly"]),
+//   }).meta({ group: "billing"})
+
+//   const address = yup.object({
+//     addr1: yup.string(),
+//     addr2: yup.string(),
+//     city: yup.string(),
+//     zip: yup.string(),
+//     webpageURL: yup.string(),
+//     gMap: yup.string()
+//   }).meta({ group: "address"})
+  
+//   const delivery = yup.object({
+//     zoneNick: yup.string().oneOf(zoneNicks),
+//     latestFirstDeliv: yup.number().lessThan(24),
+//     latestFinalDeliv: yup.number().lessThan(24),
+//     delivOrder: yup.number().integer(),
+//     specialInstructions: yup.string(),
+//     orderCnfEmail: yup.string().email(),
+//     delivOrder: yup.number(),
+//   }).meta({ group: "delivery"})
+
+//   // attributes that arent being used, or are managed automatically
+//   // in the background.
+//   const hidden = yup.object({
+//     Type: yup.string().default("Location"),
+//     createdAt: yup.string(),
+//     updatedAt: yup.string(),
+//     isActive: yup.boolean(),
+//     ttl: yup.number().integer(),
+//     locationCreditAppId: yup.string(),
+//     currentBalance: yup.number(),
+//     picURL: yup.string(),
+//   }).meta({ group: "hidden"})
+
+//   const fullSchema = nameSchema
+//     .concat(contactSchema)
+//     .concat(addressSchema)
+//     .concat(deliverySchema)
+//     .concat(billingSchema)
+//     .meta({ groups: ["Name", "Contact", "Address", "Delivery", "Billing"] })
+// }
