13d0a0dd2ee20882e005ec7cc8fadec78fa5e334
dkatosan <dkatosanma@gmail.com>
2023-06-02 08:09:09 -0700

    add csv export to database viewer

--- src/Pages/Production/NewPages/Database.jsx
+++ src/Pages/Production/NewPages/Database.jsx
@@ -626,12 +626,14 @@ import { LIST_TABLES } from "../../../data/_constants"
 import { DataTable } from "primereact/datatable"
 import { Column } from "primereact/column"
 import { MultiSelect } from "primereact/multiselect"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useListData } from "../../../data/_listData"
+import { Button } from "primereact/button"
 
 export const Database = () => {
   const [selectedTable, setSelectedTable] = useState()
   const [visibleColumns, setVisibleColumns] = useState([])
+  const dt = useRef(null)
   const { data } = useListData({ 
     tableName: selectedTable, shouldFetch: !!selectedTable
   })
@@ -640,6 +642,20 @@ export const Database = () => {
     : []
 
   useEffect(() => {setVisibleColumns([])}, [data])
+
+  const exportCSV = (selectionOnly) => {
+    dt.current.exportCSV({ selectionOnly });
+  }
+
+  const header = (
+    <div className="flex align-items-center justify-content-end gap-2">
+      <Button className="p-button-rounded"
+        type="button" 
+        icon="pi pi-file"  
+        onClick={() => exportCSV(false)} data-pr-tooltip="CSV" 
+      />  
+    </div>
+  )
 
   return(
     <div>
@@ -666,7 +682,9 @@ export const Database = () => {
       
       {data && columns.length &&
       <DataTable
+        ref={dt}
         value={data || []}
+        header={header}
         paginator rows={20}
         size="small"
         responsiveLayout="scroll"
