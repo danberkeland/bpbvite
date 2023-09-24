071356a31693d0841678fe907c97168c14f1d8af
dkatosan <dkatosanma@gmail.com>
2023-09-24 10:03:39 -0700

    commented out arithmetic features

--- src/Pages/Ordering/Orders/Components/CartComponents/CartItemDisplay/CartQtyInput.jsx
+++ src/Pages/Ordering/Orders/Components/CartComponents/CartItemDisplay/CartQtyInput.jsx
@@ -11,16 +11,20 @@ export const CartQtyInput = ({
   setCartItems, 
   disableInputs,
 }) => {
-  const [rollbackQty, setRollbackQty] = useState()
   const { prodNick, baseQty, qty } = rowData
   const { maxQty:userMaxQty, qtyChanged } = cartMeta[prodNick]
-
+  const [rollbackQty, setRollbackQty] = useState()
+  //const [inputStr, setInputStr] = useState(qty)
+  
   const maxQty = user.authClass === 'bpbfull' ? 999 : userMaxQty
   const disableQtyInput = disableInputs 
     || (maxQty === 0 && user.authClass !== 'bpbfull')
     || (!product.defaultInclude && user.authClass !== 'bpbfull')
 
   const updateProductQty = (newQty) => {
+    const inputIsValid = /^\d{0,3}$/.test(newQty)
+    if (!inputIsValid) return
+
     const updateQty = newQty === '' 
       ? ''
       : Math.min(maxQty, Number(newQty))
@@ -31,22 +35,68 @@ export const CartQtyInput = ({
     ))
   }
 
+  // const handleChange = (value) => {
+  //   const inputIsValid = /^[+-]?\d{0,3}$/.test(value)
+  //   if (!inputIsValid) return
+
+  //   console.log(value, typeof(value))
+
+  //   let newQty = 0
+  //   let newInputStr = ''
+
+  //   if (value[0] === '+') {
+  //     const addQty = value.slice(1)
+  //     newQty = Math.min(maxQty, Number(rollbackQty) + Number(addQty))
+  //     newInputStr = addQty === '' 
+  //       ? '+' 
+  //       : '+' + ((newQty - Number(rollbackQty)) || '')
+
+  //     console.log('addQty', addQty)
+
+  //   } else if (value[0] === '-') {
+  //     const subQty = value.slice(1)
+  //     newQty = Math.max(0, Number(rollbackQty) - Number(subQty))
+  //     newInputStr = value === '-'
+  //       ? '-'
+  //       : '-' + ((Number(rollbackQty) - newQty) || '')
+
+  //   } else {
+  //     newQty = Number(value)
+  //     newInputStr = value
+
+  //   }
+
+  //   // console.log('newQty:', newQty)
+  //   // console.log('newInputStr:', newInputStr)
+  //   setCartItems(cartItems.map(item => item.prodNick === prodNick
+  //     ? { ...item, qty: newQty }
+  //     : item
+  //   ))
+  //   setInputStr(newInputStr)
+
+  // }
+
+  const inputStyle = {
+    width: "62px",
+    float: "right",
+    fontSize: "1.1rem",
+    fontWeight: qtyChanged ? "bold" : "normal",
+    opacity: rowData.qty === 0 ? ".85" : "",
+    backgroundColor: qtyChanged ? 'hsl(37, 67%, 95%)' :'',
+  }
+
+  //if (!rowData) return <InputText style={inputStyle} readOnly={true} />
+
   return (<>
     <InputText
       value={qty}
+      //value={inputStr}
       readOnly={disableQtyInput}
       inputMode="numeric"
-      // keyfilter={/[0-9]/}
-      keyfilter={/\d/}
+      keyfilter={/[0-9]/}
+      //keyfilter={/[0-9+-]/}
       autoComplete="off"
-      style={{
-        width: "62px",
-        float: "right",
-        fontSize: "1.1rem",
-        fontWeight: qtyChanged ? "bold" : "normal",
-        opacity: rowData.qty === 0 ? ".85" : "",
-        backgroundColor: qtyChanged ? 'hsl(37, 67%, 95%)' :'',
-      }}
+      style={inputStyle}
       tooltip={product.packSize > 1 
         ? `= ${(rowData.qty || 0) * product.packSize} ea`
         : ''
@@ -66,29 +116,38 @@ export const CartQtyInput = ({
           setRollbackQty(parseInt(e.target.value) || 0)
         }
       }}
-      onChange={(e) => updateProductQty(e.target.value)}
+      onChange={(e) => {
+        updateProductQty(e.target.value)
+        //handleChange(e.target.value)
+      }} 
       onKeyDown={(e) => {
         //console.log(e)
         if (e.key === "Enter") { 
           e.target.blur();
-          if (e.target.value === '') updateProductQty(0)
+          if (e.target.value === '') {
+            updateProductQty(0)
+            //handleChange(0)
+          } 
         }
 
         if (e.key === "Escape") {
           if (e.target.value === '') {
             e.target.blur()
             updateProductQty(baseQty)
+            //handleChange(baseQty) 
             setRollbackQty(baseQty)
           } else {
             e.target.blur()
             updateProductQty(rollbackQty)
+            //handleChange(rollbackQty) 
           }
         }
       }}
       onBlur={() => {
         if (rowData.qty === '') {
           updateProductQty(0)
-        }
+          //handleChange(0) 
+        } //else { handleChange(qty) }
       }}
     />
     {/* <pre>{JSON.stringify([baseQty, rollbackQty])}</pre> */}
