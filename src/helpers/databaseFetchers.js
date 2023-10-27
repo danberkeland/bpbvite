5afedc2e39f5b001439ba80cd2f251657537b099
dkatosan <dkatosanma@gmail.com>
2023-10-27 14:40:33 -0700

    testing new overnight flip functions

--- src/helpers/databaseFetchers.js
+++ src/helpers/databaseFetchers.js
@@ -22,8 +22,7 @@ import { getOrdersList } from "../Pages/Production/Utils/utils";
 import ComposeNorthList from "../Pages/Logistics/utils/composeNorthList";
 import ComposeCroixInfo from "../Pages/Logistics/utils/composeCroixInfo";
 import { getTimeToLive, getTtl } from "../functions/dateAndTime";
-import { keyBy } from "lodash";
-import { isEqual } from "lodash";
+import { groupBy, keyBy, filter, flow, mapKeys, mapValues, sumBy, uniqBy, sortBy, map, identity } from "lodash/fp";
 
 const clonedeep = require("lodash.clonedeep");
 const { DateTime } = require("luxon");
@@ -471,6 +470,11 @@ export const preshapedFlip = async ({ productCache, todayDT }) => {
 
   const { data:PRD, submitMutations, updateLocalData } = productCache
 
+  if (!PRD) {
+    console.error("Cannot execute preshaped flip: Data not loaded.")
+    return
+  }
+
   const tomorrowISO = todayDT.plus({ days: 1 }).toFormat('yyyy-MM-dd')
 
   const updateInputs = PRD
@@ -492,6 +496,11 @@ export const preshapedFlip = async ({ productCache, todayDT }) => {
 export const doughFlip = async ({ doughCache, todayDT }) => {
 
   const { data:DGH, submitMutations, updateLocalData } = doughCache
+
+  if (!DGH) {
+    console.error("Cannot execute dough flip: Data not loaded.")
+    return
+  }
 
   const tomorrowISO = todayDT.plus({ days: 1 }).toFormat('yyyy-MM-dd')
 
@@ -515,19 +524,24 @@ export const syncSquareOrders = async ({ productCache, orderCache }) => {
 
   const BPBN_LOC_ID = "16VS30T9E7CM9"
 
-  const { data:PRD=[] } = productCache
+  const { data:PRD } = productCache
   const { 
-    data:ORD=[], 
+    data:ORD, 
     submitMutations:submitOrders, 
     updateLocalData:updateOrderCache 
   } = orderCache
 
-  const productsBySquareID = keyBy(PRD, 'squareID')
+  if (!PRD || !ORD) {
+    console.error("Cannot execute sync Square: data not loaded.")
+    return
+  }
+
+  const productsBySquareID = keyBy('squareID')(PRD)
   const retailOrders = ORD.filter(order => order.isWhole === false)
 
   console.log("Fetching Square Orders...")
-  const fetchURL = "https://8eo1jrov6a.execute-api.us-east-2.amazonaws.com/done"
-  const squareOrders = await fetch(fetchURL)
+  const url = "https://8eo1jrov6a.execute-api.us-east-2.amazonaws.com/done"
+  const squareOrders = await fetch(url)
     .then(response => response.json()) // .json() is an async method apparently
     .then(data => JSON.parse(data))
   console.log("Square orders:", squareOrders)
@@ -565,77 +579,382 @@ export const syncSquareOrders = async ({ productCache, orderCache }) => {
 
 
 
-const testSyncSquareOrders = ({ createCandidates, legacyItems }) => {
-  const versionsMatch = createCandidates.every((input, idx) =>
-    isEqual(createCandidates[idx], legacyItems[idx])
-  )
-  console.log("Versions produce same output?", versionsMatch)
-  console.log("createCandidates", createCandidates)
-  console.log("legacyItems", legacyItems)
-
-  return versionsMatch
+const shapeNickMap = {
+  pl: 'pl', frpl: 'pl', al: 'pl', fral: 'pl',
+  ch: 'ch', frch: 'ch',
+  pg: 'pg', frpg: 'pg',
+  sf: 'sf', frsf: 'sf',
+  mb: 'mb', frmb: 'mb', unmb: 'mb',
+  mini: 'mini', frmni: 'mini',
 }
 
-const getLegacySquareSubmitItems = ({ ord, products }) => {
-  console.log("Yes they have!  Loading new Square Orders in DB");
+export const croixCountFlip = ({ orderCache, productCache }) => {
 
-  //let ord = await fetchSq();
-  console.log("Fetched Sq orders:", ord)
-  if (ord) {
+  const { data:ORD } = orderCache   // useT0T7prodOrders
+  const { data:PRD } = productCache // useListData with table "Product"
+
+  if (!PRD || !ORD) {
+    console.error("Cannot execute croix flip: data not loaded.")
+    return
+  }
+
+  const products = keyBy('prodNick')(PRD) 
+
+  console.log("ORD:", ORD)
+  
+  const todayDT = DateTime.now().setZone('America/Los_Angeles').startOf('day')
+  // const t0 = todayDT.toFormat('yyyy-MM-dd')
+  // const t1 = todayDT.plus({ days: 1 }).toFormat('yyyy-MM-dd')
+
+  const [t0, t1, t2, t3] = [0, 1, 2, 3].map(daysAhead => 
+    todayDT.plus({ days: daysAhead + 1 }).toFormat('yyyy-MM-dd')
+  )
+
+  const adjustBackporchQty = (order) => order.locNick === "backporch" 
+    ? { ...order, qty: Math.ceil(order.qty / 2)}
+    : order
+
+  const southBakedOrders = ORD
+    .filter(order => order.delivDate === t1
+      && products[order.prodNick].doughNick === "Croissant"
+      && products[order.prodNick].packGroup === "baked pastries"
+      && order.prodNick !== 'al'
+      && order.locNick !== 'bpbextras'
+      && (
+        order.routeMeta.route.RouteDepart === "Prado"
+        || order.locNick === "backporch"
+      )
+    )
+    .map(adjustBackporchQty)
+
+  const northBakedOrders = ORD
+    .filter(order => order.delivDate === t1
+      && products[order.prodNick].doughNick === "Croissant"
+      && products[order.prodNick].packGroup === "baked pastries"
+      && order.prodNick !== 'al'
+      && order.locNick !== 'bpbextras'
+      && (
+        order.routeMeta.route.RouteDepart === "Carlton"
+        || order.locNick === "backporch"
+      )
+    )
+    .map(adjustBackporchQty)
+
+  const southFrozenOrders = ORD
+    .filter(order => order.delivDate === t0
+      && products[order.prodNick].doughNick === "Croissant"
+      && products[order.prodNick].packGroup === "frozen pastries"
+      && order.prodNick !== 'fral'
+      && order.locNick !== 'bpbextras'
+      && order.isStand !== false
+      && order.routeMeta.route.RouteDepart === "Prado"
+    )
+
+  const northFrozenOrders = ORD
+    .filter(order => order.delivDate === t0
+      && products[order.prodNick].doughNick === "Croissant"
+      && products[order.prodNick].packGroup === "frozen pastries"
+      && order.prodNick !== 'fral'
+      && order.locNick !== 'bpbextras'
+      && order.isStand !== false
+      && order.routeMeta.route.RouteDepart === "Carlton"
+    )
+
+  const [
+    southBaked, 
+    northBaked, 
+    southFrozen, 
+    northFrozen
+  ] = [
+    southBakedOrders, 
+    northBakedOrders, 
+    southFrozenOrders, 
+    northFrozenOrders
+  ].map(orders => 
+    flow(
+      groupBy(order => shapeNickMap[order.prodNick]),
+      mapValues(group => ({
+        total: sumBy('qty')(group),
+        items: group
+      }))
+    )(orders)
+  )
+  console.log("southBaked", southBaked)
+  console.log("northBaked", northBaked)
+  console.log("southFrozen", southFrozen)
+  console.log("northFrozen", northFrozen)
+
+  // applies to south freezer count, but is derived from all fral orders
+  const southFralOrders = ORD.filter(order => 
+    order.delivDate === t2 
+    && order.prodNick === 'fral'
+    && order.locNick !== 'bpbextras'
+  )
+
+  const southAlOrders = ORD.filter(order => 
+    order.delivDate === t2 
+    && order.prodNick === 'al'
+    && order.locNick !== 'bpbextras'
+    && order.routeMeta.route.RouteDepart === 'Prado'
+  )
+
+  const northAlOrders = ORD.filter(order => 
+    order.delivDate === t3 
+    && order.prodNick === 'al'
+    && order.locNick !== 'bpbextras'
+    && order.routeMeta.route.RouteDepart === 'Carlton'
+  )
+
+  const [
+    southFral, 
+    southAl, 
+    northAl,
+  ] = [
+    southFralOrders,
+    southAlOrders,
+    northAlOrders,
+  ].map(orders => 
+    flow(
+      groupBy(order => shapeNickMap[order.prodNick]),
+      mapValues(group => ({
+        total: sumBy('qty')(group),
+        items: group
+      }))
+    )(orders)
+  )
+  console.log(southFral, southAl, northAl)
+
+  const isBakedCroix = (product) => 
+    product.packGroup === "baked pastries" 
+    && product.doughNick === "Croissant"
+
+  // shapeNicks are prodNicks that get mapped to prodNicks found in the
+  // following collection. Items with the same shapeNick start off as the same
+  // shape/frozen croissant type.
+  const croixCountProducts = flow(
+    filter(isBakedCroix),
+    map(P => shapeNickMap[P.prodNick]),
+    uniqBy(identity),
+    sortBy(identity),
+    map(shapeNick => products[shapeNick]),
+    keyBy('prodNick')
+  )(PRD)
+  console.log("croixCountProducts", croixCountProducts)
+  
+  // today's opening count should be set to yesterday's closing count
+  const newFreezerNorthCounts = mapValues(P => 
+    P.freezerNorthClosing
+  )(croixCountProducts)
+
+  // predict the closing count by counting inflow vs outflow
+  const newFreezerNorthClosingCounts = mapValues(P => {
+    const frozen = northFrozen[P.prodNick]?.total ?? 0
+    const baked = northBaked[P.prodNick]?.total ?? 0
+    const stock = P.freezerNorthClosing
+    //const bakeExtra = P.bakeExtra ?? 0
+
+    const inflow =  Math.ceil(Math.max(frozen + baked - stock, 0) / 12) * 12
+    const outflow = frozen + baked
+
+    const newClosingCount = stock + inflow - outflow // + bakeExtra // not sure the bakeExtra is needed
+    return newClosingCount
+
+  })(croixCountProducts)
+
+  // today's opening count should be set to yesterday's closing count
+  const newFreezerCounts = mapValues(P => 
+    P.freezerClosing
+  )(croixCountProducts)
+
+  // predict the closing count by counting inflow vs outflow
+  // almond-type objects only have the pl attribute; count will be 0 for
+  // other products
+  const newFreezerClosingCounts = mapValues(P => {
+    const nFrozen = northFrozen[P.prodNick]?.total ?? 0
+    const nBaked = northBaked[P.prodNick]?.total ?? 0
+    const sendNorth = nFrozen + nBaked
+
+    const sFrozen = southFrozen[P.prodNick]?.total ?? 0
+    const sBaked = southBaked[P.prodNick]?.total ?? 0
+    const fral = southFral[P.prodNick]?.total ?? 0
+    const t2Al = southAl[P.prodNick]?.total ?? 0
+    const t3Al = northAl[P.prodNick]?.total ?? 0
+    const stock = P.freezerClosing
+
+    const outflow = sendNorth + sFrozen + sBaked + fral + t2Al + t3Al
+    const newClosingCount = stock - outflow
+    return newClosingCount
+
+  })(croixCountProducts) // TODO
+
+  console.log("newFreezerNorthCounts", newFreezerNorthCounts)
+  console.log("newFreezerNorthClosingCounts", newFreezerNorthClosingCounts)
+
+  console.log("newFreezerCounts", newFreezerCounts)
+  console.log("newFreezerClosingCounts", newFreezerClosingCounts)
+
+  const updateInputs = Object.values(croixCountProducts).map(P => {
+    return {
+      prodNick: P.prodNick,
+      freezerCount: newFreezerCounts[P.prodNick],
+      freezerClosing: newFreezerClosingCounts[P.prodNick],
+      freezerNorth: newFreezerNorthCounts[P.prodNick],
+      freezerNorthClosing: newFreezerNorthClosingCounts[P.prodNick],
+      sheetMake: 0,
+    }
+  })
+
+  console.log("updateInputs", updateInputs)
+  return updateInputs
+
+}
+
+
+
+// const testSyncSquareOrders = ({ createCandidates, legacyItems }) => {
+//   const versionsMatch = createCandidates.every((input, idx) =>
+//     isEqual(createCandidates[idx], legacyItems[idx])
+//   )
+//   console.log("Versions produce same output?", versionsMatch)
+//   console.log("createCandidates", createCandidates)
+//   console.log("legacyItems", legacyItems)
+
+//   return versionsMatch
+// }
+
+// const getLegacySquareSubmitItems = ({ ord, products }) => {
+//   console.log("Yes they have!  Loading new Square Orders in DB");
+
+//   //let ord = await fetchSq();
+//   console.log("Fetched Sq orders:", ord)
+//   if (ord) {
+//     let promises = []
+//     for (let newOrd of ord) {
+//       //console.log("newSqOrd", newOrd);
+//       let qty = Number(newOrd["qty"]);
+//       let dt = new Date().toISOString();
+//       //console.log("dt", dt);
+//       let deliv = newOrd["delivDate"];
+//       let delivISO = new Date(deliv);
+//       let delivDate = deliv.split("T")[0];
+
+//       let locIDBPBN = "16VS30T9E7CM9";
+
+//       let rt = "slopick";
+//       let locNick = newOrd["custName"] + "__" + newOrd["id"];
+//       let prodNick;
+//       let prodName;
+//       try {
+//         prodNick =
+//           products[
+//             products.findIndex((prod) =>
+//               newOrd["item"].includes(prod.squareID)
+//             )
+//           ]["nickName"];
+//         prodName =
+//           products[
+//             products.findIndex((prod) =>
+//               newOrd["item"].includes(prod.squareID)
+//             )
+//           ]["prodName"];
+//       } catch {
+//         prodName="Brownie"
+//         prodNick = "brn";
+//       }
+
+//       if (newOrd.location === locIDBPBN) {
+//         rt = "atownpick";
+//       }
+
+//       let itemToAdd = {
+//         qty: qty,
+//         //updatedOn: updateTimestamp,
+//         isWhole: false,
+//         ItemNote: "paid",
+//         delivDate: delivDate,
+//         locNick: locNick,
+//         prodNick: prodNick,
+//         route: rt,
+//         ttl: delivDate ? getTtl(delivISO) : null,
+//         updatedBy: "bpb_admin",
+//       };
+
+//       promises.push(itemToAdd)
+//     }
+
+//     return promises
+//   }
+// }
+
+
+
+
+
+
+export const testLegacyCroixFlip = (db) => {
+
+    const [products, customers, routes, standing, orders, doughs, altPricing] =
+    db;
+
+    console.log("Checking if Orders Have been changed");
+    let prodsToUpdate = clonedeep(products);
+
+    let bakedOrdersList = getOrdersList(tomBasedOnDelivDate(tomorrow), db);
+
+    bakedOrdersList = prodsToUpdate.filter((frz) =>
+      NorthCroixBakeFilter(frz)
+    );
+    console.log("bakedOrdersList", bakedOrdersList);
+
+    let frozenDelivsArray = compose.getFrozensLeavingCarlton(tomorrow, db);
+    let setOutArray = compose.getBakedTomorrowAtCarlton(tomorrow, db);
+
     let promises = []
-    for (let newOrd of ord) {
-      //console.log("newSqOrd", newOrd);
-      let qty = Number(newOrd["qty"]);
-      let dt = new Date().toISOString();
-      //console.log("dt", dt);
-      let deliv = newOrd["delivDate"];
-      let delivISO = new Date(deliv);
-      let delivDate = deliv.split("T")[0];
 
-      let locIDBPBN = "16VS30T9E7CM9";
+    for (let prod of bakedOrdersList) {
+      prod.freezerNorth = prod.freezerNorthClosing;
 
-      let rt = "slopick";
-      let locNick = newOrd["custName"] + "__" + newOrd["id"];
-      let prodNick;
-      let prodName;
+      let frozenDeliv;
       try {
-        prodNick =
-          products[
-            products.findIndex((prod) =>
-              newOrd["item"].includes(prod.squareID)
-            )
-          ]["nickName"];
-        prodName =
-          products[
-            products.findIndex((prod) =>
-              newOrd["item"].includes(prod.squareID)
-            )
-          ]["prodName"];
+        frozenDeliv =
+          frozenDelivsArray[
+            frozenDelivsArray.findIndex((fr) => fr.prod === prod.prodNick)
+          ].qty;
       } catch {
-        prodName="Brownie"
-        prodNick = "brn";
+        frozenDeliv = 0;
+      }
+      let setOut;
+      try {
+        setOut =
+          setOutArray[
+            setOutArray.findIndex((set) => set.prod === prod.prodNick)
+          ].qty;
+      } catch {
+        setOut = 0;
       }
 
-      if (newOrd.location === locIDBPBN) {
-        rt = "atownpick";
-      }
+      prod.freezerNorthClosing =
+        prod.freezerNorthClosing +
+        Math.ceil((setOut + frozenDeliv - prod.freezerNorthClosing) / 12) *
+          12 -
+        setOut -
+        frozenDeliv +
+        Number(prod.bakeExtra);
 
-      let itemToAdd = {
-        qty: qty,
-        //updatedOn: updateTimestamp,
-        isWhole: false,
-        ItemNote: "paid",
-        delivDate: delivDate,
-        locNick: locNick,
-        prodNick: prodNick,
-        route: rt,
-        ttl: delivDate ? getTtl(delivISO) : null,
-        updatedBy: "bpb_admin",
+      prod.freezerNorthFlag = tomorrow;
+      let prodToUpdate = {
+        prodNick: prod.nickName,
+        freezerNorth: prod.freezerNorth,
+        freezerCount: prod.freezerClosing,
+        freezerNorthClosing: prod.freezerNorthClosing,
+        freezerNorthFlag: prod.freezerNorthFlag,
+        sheetMake: 0,
       };
 
-      promises.push(itemToAdd)
+      promises.push(prodToUpdate)
+
     }
 
+    console.log(promises)
     return promises
-  }
 }
