a0d2bbd3a3310f590ee75e0988416b4b6fa00801
dkatosan <dkatosanma@gmail.com>
2023-12-17 08:48:34 -0800

    building route assignment stuff

--- src/Pages/Production/NewPages/TestPages/OrderDomain.jsx
+++ src/Pages/Production/NewPages/TestPages/OrderDomain.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { getTodayDT } from "../BPBN/utils"
 import { useCombinedOrdersByDelivDate, useCombinedOrdersByLoc } from "../../../../data/orders/combinedOrders"
+import { useListData } from "../../../../data/_listData"
 
 
 
@@ -10,11 +11,18 @@ export const OrderDomain = () => {
 
   const today = getTodayDT().toFormat('yyyy-MM-dd')
   // const combinedOrders = useCombinedOrdersByDelivDate({ delivDate: today })
-  const combinedOrders = useCombinedOrdersByLoc({ locNick: 'sotos' })
   
-  console.log(combinedOrders)
+  // const combinedOrders = useCombinedOrdersByLoc({ locNick: 'sotos' })
+  // console.log(combinedOrders)
+
+  // const { data:products } = useRoutedProducts({ locNick: 'slopro' })
+
+
+
   
   return (
     <h1>Testing Orders</h1>
   )
 }
+
+
