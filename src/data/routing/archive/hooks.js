1fe0fab0cf2e702b0b8f328fc8b4f9400dc22260
dkatosan <dkatosanma@gmail.com>
2024-02-20 23:59:28 -0800

    working on data stuff

--- src/data/routing/archive/hooks.js
+++ src/data/routing/archive/hooks.js
@@ -1,118 +1,118 @@
-// Route Assignment is powered by table data that is slightly customized on the
-// front end. typedefs here specify the shape.
-//
-// Types that aren't 100% the same as fetched from the DB are prefixed with
-// 'Routing' to signify a special usage for this context
+// // Route Assignment is powered by table data that is slightly customized on the
+// // front end. typedefs here specify the shape.
+// //
+// // Types that aren't 100% the same as fetched from the DB are prefixed with
+// // 'Routing' to signify a special usage for this context
 
-import { useListData } from "../../_listData.js"
-import { locationAttributes } from "../../../customGraphQL/standardAttributes.js"
-import { groupBy } from "lodash/fp"
+// import { useListData } from "../../_listData.js"
+// import { locationAttributes } from "../../../customGraphQL/standardAttributes.js"
+// import { groupBy } from "lodash/fp"
 
-import { RoutingLocation, _RoutingLocation, RoutingProduct, _RoutingProduct, RoutingRoute, _RoutingRoute, ZoneRoute } from "./types.d.js"
-import useSWR from "swr"
-import gqlFetcher from "../../_fetchers.js"
-import { defaultSwrOptions } from "../../_constants.js"
+// import { RoutingLocation, _RoutingLocation, RoutingProduct, _RoutingProduct, RoutingRoute, _RoutingRoute, ZoneRoute } from "./types.d.js"
+// import useSWR from "swr"
+// import gqlFetcher from "../../_fetchers.js"
+// import { defaultSwrOptions } from "../../_constants.js"
 
-/** @typedef {boolean[]} WeekdayFlags */
+// /** @typedef {boolean[]} WeekdayFlags */
 
-/**
- * @param {RoutingLocation} location 
- * @returns {_RoutingLocation}
- */
-const to_Location = location => ({
-  locNick: location.locNick,
-  zoneNick: location.zoneNick,
-  delivTimeStart: location.latestFirstDeliv,
-  delivTimeFinis: location.latestFinalDeliv
-})
+// /**
+//  * @param {RoutingLocation} location 
+//  * @returns {_RoutingLocation}
+//  */
+// const to_Location = location => ({
+//   locNick: location.locNick,
+//   zoneNick: location.zoneNick,
+//   delivTimeStart: location.latestFirstDeliv,
+//   delivTimeFinis: location.latestFinalDeliv
+// })
 
-/**
- * @param {RoutingProduct} product 
- * @returns {_RoutingProduct}
- */
-const to_Product = product => ({
-  prodNick: product.prodNick,
-  bakedWhere: product.bakedWhere,
-  readyTime: product.readyTime,
-  // @ts-ignore
-  validDays: product.daysAvailable?.map(dValue => !!dValue) 
-    || [true,true,true,true,true,true,true]
-})
+// /**
+//  * @param {RoutingProduct} product 
+//  * @returns {_RoutingProduct}
+//  */
+// const to_Product = product => ({
+//   prodNick: product.prodNick,
+//   bakedWhere: product.bakedWhere,
+//   readyTime: product.readyTime,
+//   // @ts-ignore
+//   validDays: product.daysAvailable?.map(dValue => !!dValue) 
+//     || [true,true,true,true,true,true,true]
+// })
 
-const legacyWeekdays = ['1','2','3','4','5','6','7']
+// const legacyWeekdays = ['1','2','3','4','5','6','7']
 
-/** 
- * @param {RoutingRoute[]} routes
- * @param {ZoneRoute[]} zoneRoutes 
- * @returns {_RoutingRoute[]}
- */
-const to_Routes = (routes, zoneRoutes) => {
-  const zrByRouteNick = groupBy('routeNick')(zoneRoutes)
+// /** 
+//  * @param {RoutingRoute[]} routes
+//  * @param {ZoneRoute[]} zoneRoutes 
+//  * @returns {_RoutingRoute[]}
+//  */
+// const to_Routes = (routes, zoneRoutes) => {
+//   const zrByRouteNick = groupBy('routeNick')(zoneRoutes)
 
-  return routes.map(route => ({
-    routeNick: route.routeNick,
-    timeStart: route.routeStart,
-    timeFinis: route.routeStart + route.routeTime,
-    validDays: legacyWeekdays.map(wd => route.RouteSched.includes(wd)),
-    hubStart: route.RouteDepart,
-    hubFinis: route.RouteArrive,
-    zonesServed: zrByRouteNick[route.routeNick]?.map(zr => zr.zoneNick) ?? []
-  }))
+//   return routes.map(route => ({
+//     routeNick: route.routeNick,
+//     timeStart: route.routeStart,
+//     timeFinis: route.routeStart + route.routeTime,
+//     validDays: legacyWeekdays.map(wd => route.RouteSched.includes(wd)),
+//     hubStart: route.RouteDepart,
+//     hubFinis: route.RouteArrive,
+//     zonesServed: zrByRouteNick[route.routeNick]?.map(zr => zr.zoneNick) ?? []
+//   }))
 
-}
+// }
 
-/** 
- * @typedef {Object} ZoneRoute
- * @property {string} id
- * @property {string} zoneNick
- * @property {string} routeNick
- */
+// /** 
+//  * @typedef {Object} ZoneRoute
+//  * @property {string} id
+//  * @property {string} zoneNick
+//  * @property {string} routeNick
+//  */
 
 
-/** @returns {_RoutingRoute[]} */
-const useRoutes = () => {
-  const { data:RTE=[] } = useListData({ tableName: "Route", shouldFetch: true })
-  const { data:ZRT=[] } = useListData({ tableName: "ZoneRoute", shouldFetch: true })
+// /** @returns {_RoutingRoute[]} */
+// const useRoutes = () => {
+//   const { data:RTE=[] } = useListData({ tableName: "Route", shouldFetch: true })
+//   const { data:ZRT=[] } = useListData({ tableName: "ZoneRoute", shouldFetch: true })
 
-  return to_Routes(RTE, ZRT)
-}
+//   return to_Routes(RTE, ZRT)
+// }
 
-const useLocation = ({ locNick }) => {
-  const query = /* GraphQL */ `
-    query GetLocation($locNick: String!) {
-      getLocation(locNick: $locNick) {
-        ${locationAttributes}
-      }
-    }
-  `
-  const variables = { locNick }
-  const key = [query, variables]
+// const useLocation = ({ locNick }) => {
+//   const query = /* GraphQL */ `
+//     query GetLocation($locNick: String!) {
+//       getLocation(locNick: $locNick) {
+//         ${locationAttributes}
+//       }
+//     }
+//   `
+//   const variables = { locNick }
+//   const key = [query, variables]
 
-  const { data } = useSWR(key, gqlFetcher, defaultSwrOptions)
+//   const { data } = useSWR(key, gqlFetcher, defaultSwrOptions)
 
-  return data ? to_Location(data.data.getLocation) : undefined
+//   return data ? to_Location(data.data.getLocation) : undefined
   
-}
+// }
 
-/** @returns {_RoutingLocation[]} */
-const useLocations = () => {
-  const { data:LOC=[] } = useListData({ tableName: "Location", shouldFetch: true })
+// /** @returns {_RoutingLocation[]} */
+// const useLocations = () => {
+//   const { data:LOC=[] } = useListData({ tableName: "Location", shouldFetch: true })
 
-  return LOC.map(to_Location)
-}
+//   return LOC.map(to_Location)
+// }
 
-/** @returns {_RoutingProduct[]} */
-const useProducts = () => {
-  const { data:PRD=[] } = useListData({ tableName: "Product", shouldFetch: true})
+// /** @returns {_RoutingProduct[]} */
+// const useProducts = () => {
+//   const { data:PRD=[] } = useListData({ tableName: "Product", shouldFetch: true})
 
-  return PRD.map(to_Product)
-}
+//   return PRD.map(to_Product)
+// }
 
-/** Data fetchers that prep raw DB data for use with routing logic */
-export const useRoutingData = {
-  routes: useRoutes,
-  location: useLocation,
-  locations: useLocations,
-  products: useProducts,
-}
+// /** Data fetchers that prep raw DB data for use with routing logic */
+// export const useRoutingData = {
+//   routes: useRoutes,
+//   location: useLocation,
+//   locations: useLocations,
+//   products: useProducts,
+// }
 
--- src/data/routing/archive/routeAssignment.js
+++ src/data/routing/archive/routeAssignment.js
@@ -1,305 +1,305 @@
-// module to house route assignment logic
+// // module to house route assignment logic
 
-import { flow, groupBy, keyBy, mapValues, sortBy } from "lodash/fp"
-import { useRoutingData } from "./hooks"
+// import { flow, groupBy, keyBy, mapValues, sortBy } from "lodash/fp"
+// import { useRoutingData } from "./hooks"
 
-import { RoutingLocation, _RoutingLocation, RoutingProduct, _RoutingProduct, RoutingRoute, _RoutingRoute, ZoneRoute } from "./types.d.js"
-import { useMemo } from "react"
-import { HOLIDAYS } from "../../../constants/constants.js"
-
-
-
-const modulo = (n, m) => ((n % m) + m) % m
-
-// Assumes intervals are closed on both ends, '[ , ]' - style.
-// Better setup would be to use half open, '[ , )' - style intervals, but
-// this matches prior logic more cleanly.
-const intervalContainsPoint = (I, point) => I[0] <= point && point <= I[1]
-const intervalsIntersect = (I1, I2) => 
-  intervalContainsPoint(I1, I2[0])
-  || intervalContainsPoint(I2, I1[0])
+// import { RoutingLocation, _RoutingLocation, RoutingProduct, _RoutingProduct, RoutingRoute, _RoutingRoute, ZoneRoute } from "./types.d.js"
+// import { useMemo } from "react"
+// import { HOLIDAYS } from "../../../constants/constants.js"
 
 
-const ifValidDo = (testFn, ...args) => {
-  return (result) => result.error
-    ? result
-    : { ...result, ...testFn(...args) }
-}
 
-const routingErrorMsgs = {
-  1: "route does not serve zone",
-  2: "location not available during delivery",
-  3: "transfer route not found",
-  4: "could not schedule transfer in time",
-  5: "could not schedule product in time",
-  6: "route not scheduled on weekday",
-  7: "closed for holiday",
-}
+// const modulo = (n, m) => ((n % m) + m) % m
 
-const testDay = (route, weekday, delivDate) => {
-  const weekdayError = route.validDays[weekday] ? null : 6
-  const holidayError = !HOLIDAYS.includes(delivDate) ? null : 7
-  const error = weekdayError || holidayError || null
+// // Assumes intervals are closed on both ends, '[ , ]' - style.
+// // Better setup would be to use half open, '[ , )' - style intervals, but
+// // this matches prior logic more cleanly.
+// const intervalContainsPoint = (I, point) => I[0] <= point && point <= I[1]
+// const intervalsIntersect = (I1, I2) => 
+//   intervalContainsPoint(I1, I2[0])
+//   || intervalContainsPoint(I2, I1[0])
+
+
+// const ifValidDo = (testFn, ...args) => {
+//   return (result) => result.error
+//     ? result
+//     : { ...result, ...testFn(...args) }
+// }
+
+// const routingErrorMsgs = {
+//   1: "route does not serve zone",
+//   2: "location not available during delivery",
+//   3: "transfer route not found",
+//   4: "could not schedule transfer in time",
+//   5: "could not schedule product in time",
+//   6: "route not scheduled on weekday",
+//   7: "closed for holiday",
+// }
+
+// const testDay = (route, weekday, delivDate) => {
+//   const weekdayError = route.validDays[weekday] ? null : 6
+//   const holidayError = !HOLIDAYS.includes(delivDate) ? null : 7
+//   const error = weekdayError || holidayError || null
   
-  return { error }
-}
+//   return { error }
+// }
 
-const testZone = (route, location) => {
-  const isValid = route.zonesServed.includes(location.zoneNick)
-  return { error: isValid ? null : 1 }
-}
+// const testZone = (route, location) => {
+//   const isValid = route.zonesServed.includes(location.zoneNick)
+//   return { error: isValid ? null : 1 }
+// }
 
-const testTimePeriod = (route, location) => {
-  const isValid = intervalsIntersect(
-    [route.timeStart, route.timeFinis],
-    [location.delivTimeStart, location.delivTimeFinis]
-  )
+// const testTimePeriod = (route, location) => {
+//   const isValid = intervalsIntersect(
+//     [route.timeStart, route.timeFinis],
+//     [location.delivTimeStart, location.delivTimeFinis]
+//   )
 
-  return { error: isValid ? null : 2 }
-}
+//   return { error: isValid ? null : 2 }
+// }
 
-/**
- * 
- * @param {_RoutingRoute} route 
- * @param {_RoutingProduct} product 
- * @param {number} weekday
- * @param {_RoutingRoute[]} transferRoutes 
- */
-const getFulfillmentPlan = (route, product, weekday, transferRoutes) => {
-  const fulfillEvent = { 
-    ...route, 
-    name: "fulfillment",
-    isValid: true,
-    weekday, 
-    dateOffset: 0
-  }
+// /**
+//  * 
+//  * @param {_RoutingRoute} route 
+//  * @param {_RoutingProduct} product 
+//  * @param {number} weekday
+//  * @param {_RoutingRoute[]} transferRoutes 
+//  */
+// const getFulfillmentPlan = (route, product, weekday, transferRoutes) => {
+//   const fulfillEvent = { 
+//     ...route, 
+//     name: "fulfillment",
+//     isValid: true,
+//     weekday, 
+//     dateOffset: 0
+//   }
 
-  const bakeHub = product.bakedWhere.includes(route.hubStart)
-    ? route.hubStart
-    : product.bakedWhere[0]
+//   const bakeHub = product.bakedWhere.includes(route.hubStart)
+//     ? route.hubStart
+//     : product.bakedWhere[0]
 
-  const needsTransfer = bakeHub !== route.hubFinis
-  const transferRoute = needsTransfer 
-    ? transferRoutes.find(tR =>
-        tR.hubStart === bakeHub && tR.hubFinis === route.hubFinis
-      )
-    : null
+//   const needsTransfer = bakeHub !== route.hubFinis
+//   const transferRoute = needsTransfer 
+//     ? transferRoutes.find(tR =>
+//         tR.hubStart === bakeHub && tR.hubFinis === route.hubFinis
+//       )
+//     : null
 
-  if (transferRoute === undefined) { // couldn't make connection
-    const fulfillmentPlan = [
-      { 
-        routeNick: 'NOT ASSIGNED', 
-        timeStart: 0,
-        timeFinis: 0,
-        validDays: [false, false, false, false, false, false, false],
-        hubStart: bakeHub,
-        hubFinis: route.hubStart,
-        zonesServed: [],
-        name: "transfer",
-        isValid: false, 
-        weekday,
-        dateOffset: 0,
-      },
-      { ...fulfillEvent }
-    ]
+//   if (transferRoute === undefined) { // couldn't make connection
+//     const fulfillmentPlan = [
+//       { 
+//         routeNick: 'NOT ASSIGNED', 
+//         timeStart: 0,
+//         timeFinis: 0,
+//         validDays: [false, false, false, false, false, false, false],
+//         hubStart: bakeHub,
+//         hubFinis: route.hubStart,
+//         zonesServed: [],
+//         name: "transfer",
+//         isValid: false, 
+//         weekday,
+//         dateOffset: 0,
+//       },
+//       { ...fulfillEvent }
+//     ]
 
-    return { error: 3, fulfillmentPlan }
-  }
+//     return { error: 3, fulfillmentPlan }
+//   }
 
-  if (transferRoute === null) { // no transfer needed
-    return { fulfillmentPlan: [{ ...fulfillEvent }] }
+//   if (transferRoute === null) { // no transfer needed
+//     return { fulfillmentPlan: [{ ...fulfillEvent }] }
 
-  }
+//   }
 
-  // need to schedule/validate transfer
-  const canScheduleSameDay = transferRoute.validDays[fulfillEvent.weekday] === true
-    && transferRoute.timeFinis < route.timeStart
+//   // need to schedule/validate transfer
+//   const canScheduleSameDay = transferRoute.validDays[fulfillEvent.weekday] === true
+//     && transferRoute.timeFinis < route.timeStart
 
-  const canSchedulePreviousDay = 
-    transferRoute.validDays[modulo(fulfillEvent.weekday - 1, 7)] === true
+//   const canSchedulePreviousDay = 
+//     transferRoute.validDays[modulo(fulfillEvent.weekday - 1, 7)] === true
 
-  const daysBeforeNextStep = canScheduleSameDay ? 0
-    : canSchedulePreviousDay ? 1
-    : 0
+//   const daysBeforeNextStep = canScheduleSameDay ? 0
+//     : canSchedulePreviousDay ? 1
+//     : 0
 
-  const fulfillmentPlan = [
-    { 
-      ...transferRoute, 
-      name: "transfer",
-      isValid: canScheduleSameDay || canSchedulePreviousDay, 
-      weekday: modulo(fulfillEvent.weekday - daysBeforeNextStep, 7),
-      dateOffset: fulfillEvent.dateOffset - daysBeforeNextStep
-    },
-    { ...fulfillEvent }
-  ]
+//   const fulfillmentPlan = [
+//     { 
+//       ...transferRoute, 
+//       name: "transfer",
+//       isValid: canScheduleSameDay || canSchedulePreviousDay, 
+//       weekday: modulo(fulfillEvent.weekday - daysBeforeNextStep, 7),
+//       dateOffset: fulfillEvent.dateOffset - daysBeforeNextStep
+//     },
+//     { ...fulfillEvent }
+//   ]
 
-  return { 
-    error: fulfillmentPlan[0].isValid ? null : 4,
-    fulfillmentPlan
-  }
+//   return { 
+//     error: fulfillmentPlan[0].isValid ? null : 4,
+//     fulfillmentPlan
+//   }
 
 
-}
+// }
 
-const getProductionPlan = (product, fulfillmentPlan) => {
-  //console.log("fulfillmentPlan", fulfillmentPlan)
-  if (!fulfillmentPlan.length) return { error: "no fulfillment plan" }
-  const canScheduleSameDay = product.validDays[fulfillmentPlan[0].weekday] 
-    && product.readyTime < fulfillmentPlan[0].timeStart
+// const getProductionPlan = (product, fulfillmentPlan) => {
+//   //console.log("fulfillmentPlan", fulfillmentPlan)
+//   if (!fulfillmentPlan.length) return { error: "no fulfillment plan" }
+//   const canScheduleSameDay = product.validDays[fulfillmentPlan[0].weekday] 
+//     && product.readyTime < fulfillmentPlan[0].timeStart
 
-  const canSchedulePreviousDay = 
-    product.validDays[modulo(fulfillmentPlan[0].weekday - 1, 7)] === true
+//   const canSchedulePreviousDay = 
+//     product.validDays[modulo(fulfillmentPlan[0].weekday - 1, 7)] === true
 
-  const daysBeforeNextStep = canScheduleSameDay ? 0
-    : canSchedulePreviousDay ? 1
-    : 0
+//   const daysBeforeNextStep = canScheduleSameDay ? 0
+//     : canSchedulePreviousDay ? 1
+//     : 0
 
-  const productionPlan = [{
-    ...product,
-    name: "finished",
-    isValid: canScheduleSameDay || canSchedulePreviousDay,
-    weekday: modulo(fulfillmentPlan[0].weekday - daysBeforeNextStep, 7),
-    dateOffset: fulfillmentPlan[0].dateOffset - daysBeforeNextStep
-  }]
+//   const productionPlan = [{
+//     ...product,
+//     name: "finished",
+//     isValid: canScheduleSameDay || canSchedulePreviousDay,
+//     weekday: modulo(fulfillmentPlan[0].weekday - daysBeforeNextStep, 7),
+//     dateOffset: fulfillmentPlan[0].dateOffset - daysBeforeNextStep
+//   }]
   
-  
-
-  return {
-    error: productionPlan[0].isValid ? null : 5,
-    fulfillmentLeadTime: productionPlan[0].dateOffset * -1,
-    productionPlan
-  }
-}
-
-
-/**
- * Main Routing Logic: "Decides" what the production/fulfillment plan should be,
- * and validates the plan
- * @param {Object} input
- * @param {_RoutingLocation} input.location 
- * @param {_RoutingProduct} input.product
- * @param {_RoutingRoute} input.route
- * @param {number} input.weekday
- * @param {string|null} input.delivDate
- * @param {_RoutingRoute[]} input.transferRoutes
- */
-const getRouteSummary = ({
-  location,
-  product,
-  route,
-  weekday,
-  delivDate=null,
-  transferRoutes
-}) => {
-
-  const initialResult = {
-    routeNick: route.routeNick,
-    error: null,
-    fulfillmentLeadTime: 0,
-    productionPlan: [],
-    fulfillmentPlan: [],
-  }
-
-  const finalSummary = flow(
-    ifValidDo(testDay, route, weekday, delivDate),
-    ifValidDo(testZone, route, location),
-    ifValidDo(testTimePeriod, route, location),
-    ifValidDo(getFulfillmentPlan, route, product, weekday, transferRoutes),
-    result => ifValidDo(getProductionPlan, product, result.fulfillmentPlan)(result)
-  )(initialResult)
-
-  return finalSummary
-}
-
-
-
-/**
- * One of the main logic units for route assignment. Returns valid fulfillment
- * plans, plus a final production event (baking), along with date info to aid
- * work orchestration.
- * @param {Object} input
- * @param {_RoutingLocation} input.location 
- * @param {_RoutingProduct} input.product
- * @param {number} input.weekday
- * @param {string|null} input.delivDate
- * @param {_RoutingRoute[]} input.routes
- */
-export const getRouteOptions = ({
-  location,
-  product,
-  weekday,
-  delivDate,
-  routes,
-}) => {
-
-  const transferRoutes = routes.filter(route => route.hubStart !== route.hubFinis)
-  const delivRoutes = routes.filter(route => !route.routeNick.includes("Pick up"))
-  const pickupRoutes = routes.filter(route => route.routeNick.includes("Pick up"))
-
-  const delivSummaries = delivRoutes.map(route => getRouteSummary({
-    location, product, route, weekday, delivDate, transferRoutes
-  }))
-
-  const pickupSummaries = pickupRoutes.map(route => getRouteSummary({
-    location: { 
-      locNick: route.zonesServed[0],
-      zoneNick: route.zonesServed[0],
-      delivTimeStart: 7,
-      delivTimeFinis: 11,
-    },
-    product,
-    route,
-    weekday,
-    delivDate,
-    transferRoutes
-  }))
-
-  return {
-    deliv: sortBy([
-      summary => summary.error === null ? 0 : 1, 
-      summary => summary.productionPlan.at(0).dateOffset * -1,
-      summary => summary.fulfillmentPlan.at(-1).timeStart
-    ])(delivSummaries.filter(summary => summary.error === null)),
-    ...keyBy('routeNick')(pickupSummaries)
-  }
-
-}
-
-
-
-
-/**
- * Returns a function tailored for the input locNick, with specific location
- * data, plus product & route data pre-bound. The returned function produces
- * produciton/fulfillment options for the location given an input product
- * and weekday number.
- */
-export const useGetRouteOptionsByLocation = (locNick) => {
-  const location = useRoutingData.location({ locNick })
-  const products = useRoutingData.products()
-  const routes = useRoutingData.routes()
-
-  const buildFunction = () => {
-    if (!location || !products || !routes) return undefined
-
-    return (prodNick, weekday, delivDate) => {
-      const product = products.find(P => P.prodNick === prodNick)
-
-      if (!product) return undefined
-      else return getRouteOptions({
-        location, 
-        product, 
-        weekday, 
-        delivDate,
-        routes
-      })
-
-    }
-
-
-  }
-
-  return useMemo(buildFunction, [locNick, location, products, routes])
   
 
-}
+//   return {
+//     error: productionPlan[0].isValid ? null : 5,
+//     fulfillmentLeadTime: productionPlan[0].dateOffset * -1,
+//     productionPlan
+//   }
+// }
+
+
+// /**
+//  * Main Routing Logic: "Decides" what the production/fulfillment plan should be,
+//  * and validates the plan
+//  * @param {Object} input
+//  * @param {_RoutingLocation} input.location 
+//  * @param {_RoutingProduct} input.product
+//  * @param {_RoutingRoute} input.route
+//  * @param {number} input.weekday
+//  * @param {string|null} input.delivDate
+//  * @param {_RoutingRoute[]} input.transferRoutes
+//  */
+// const getRouteSummary = ({
+//   location,
+//   product,
+//   route,
+//   weekday,
+//   delivDate=null,
+//   transferRoutes
+// }) => {
+
+//   const initialResult = {
+//     routeNick: route.routeNick,
+//     error: null,
+//     fulfillmentLeadTime: 0,
+//     productionPlan: [],
+//     fulfillmentPlan: [],
+//   }
+
+//   const finalSummary = flow(
+//     ifValidDo(testDay, route, weekday, delivDate),
+//     ifValidDo(testZone, route, location),
+//     ifValidDo(testTimePeriod, route, location),
+//     ifValidDo(getFulfillmentPlan, route, product, weekday, transferRoutes),
+//     result => ifValidDo(getProductionPlan, product, result.fulfillmentPlan)(result)
+//   )(initialResult)
+
+//   return finalSummary
+// }
+
+
+
+// /**
+//  * One of the main logic units for route assignment. Returns valid fulfillment
+//  * plans, plus a final production event (baking), along with date info to aid
+//  * work orchestration.
+//  * @param {Object} input
+//  * @param {_RoutingLocation} input.location 
+//  * @param {_RoutingProduct} input.product
+//  * @param {number} input.weekday
+//  * @param {string|null} input.delivDate
+//  * @param {_RoutingRoute[]} input.routes
+//  */
+// export const getRouteOptions = ({
+//   location,
+//   product,
+//   weekday,
+//   delivDate,
+//   routes,
+// }) => {
+
+//   const transferRoutes = routes.filter(route => route.hubStart !== route.hubFinis)
+//   const delivRoutes = routes.filter(route => !route.routeNick.includes("Pick up"))
+//   const pickupRoutes = routes.filter(route => route.routeNick.includes("Pick up"))
+
+//   const delivSummaries = delivRoutes.map(route => getRouteSummary({
+//     location, product, route, weekday, delivDate, transferRoutes
+//   }))
+
+//   const pickupSummaries = pickupRoutes.map(route => getRouteSummary({
+//     location: { 
+//       locNick: route.zonesServed[0],
+//       zoneNick: route.zonesServed[0],
+//       delivTimeStart: 7,
+//       delivTimeFinis: 11,
+//     },
+//     product,
+//     route,
+//     weekday,
+//     delivDate,
+//     transferRoutes
+//   }))
+
+//   return {
+//     deliv: sortBy([
+//       summary => summary.error === null ? 0 : 1, 
+//       summary => summary.productionPlan.at(0).dateOffset * -1,
+//       summary => summary.fulfillmentPlan.at(-1).timeStart
+//     ])(delivSummaries.filter(summary => summary.error === null)),
+//     ...keyBy('routeNick')(pickupSummaries)
+//   }
+
+// }
+
+
+
+
+// /**
+//  * Returns a function tailored for the input locNick, with specific location
+//  * data, plus product & route data pre-bound. The returned function produces
+//  * produciton/fulfillment options for the location given an input product
+//  * and weekday number.
+//  */
+// export const useGetRouteOptionsByLocation = (locNick) => {
+//   const location = useRoutingData.location({ locNick })
+//   const products = useRoutingData.products()
+//   const routes = useRoutingData.routes()
+
+//   const buildFunction = () => {
+//     if (!location || !products || !routes) return undefined
+
+//     return (prodNick, weekday, delivDate) => {
+//       const product = products.find(P => P.prodNick === prodNick)
+
+//       if (!product) return undefined
+//       else return getRouteOptions({
+//         location, 
+//         product, 
+//         weekday, 
+//         delivDate,
+//         routes
+//       })
+
+//     }
+
+
+//   }
+
+//   return useMemo(buildFunction, [locNick, location, products, routes])
+  
+
+// }
