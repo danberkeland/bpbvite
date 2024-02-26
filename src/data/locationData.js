c5da242825067802ae009ec664ff3c08ce45d8af
dkatosan <dkatosanma@gmail.com>
2024-02-26 11:26:38 -0800

    comment out unused code

--- src/data/locationData.js
+++ src/data/locationData.js
@@ -14,29 +14,29 @@ import gqlFetcher from "./_fetchers"
 import * as yup from "yup"
 import { sortBy } from "lodash"
 
-const listLocationsSimple = /* GraphQL */ `
-  query ListLocations(
-    $locNick: String
-    $filter: ModelLocationFilterInput
-    $limit: Int
-    $nextToken: String
-    $sortDirection: ModelSortDirection
-  ) {
-    listLocations(
-      locNick: $locNick
-      filter: $filter
-      limit: $limit
-      nextToken: $nextToken
-      sortDirection: $sortDirection
-    ) {
-      items {
-        locNick
-        locName
-        ttl
-      }
-    }
-  }
-`;
+// const listLocationsSimple = /* GraphQL */ `
+//   query ListLocations(
+//     $locNick: String
+//     $filter: ModelLocationFilterInput
+//     $limit: Int
+//     $nextToken: String
+//     $sortDirection: ModelSortDirection
+//   ) {
+//     listLocations(
+//       locNick: $locNick
+//       filter: $filter
+//       limit: $limit
+//       nextToken: $nextToken
+//       sortDirection: $sortDirection
+//     ) {
+//       items {
+//         locNick
+//         locName
+//         ttl
+//       }
+//     }
+//   }
+// `;
 
 const listLocationsFull = /* GraphQL */ `
   query ListLocations(
@@ -318,47 +318,6 @@ const deleteLocationMutation = /* GraphQL */ `
 //  * QUERIES/CACHES *
 //  ******************/
 
-// /**
-//  * Produces a full list of locNicks/locNames.
-//  * @param {boolean} shouldFetch Fetches data only when true.
-//  * @returns {{ data: Array<{ locNick: string, locName: string }>, errors: object }} A list of locNick ID's and locName text labels.
-//  */
-// export const useLocationListSimple = (shouldFetch) => {
-//   const { data, errors, isValidating } = useSWR(
-//     shouldFetch ? [queries.listLocationsSimple, { limit: 1000 }] : null, 
-//     gqlFetcher, 
-//     defaultSwrOptions
-//   )
-
-//   const transformData = () => {
-//     if (!data) return undefined
-//     return getNestedObject(data, ['data', 'listLocations', 'items']).sort(dynamicSort("locName"))
-//   }
-//   const _data = useMemo(transformData, [data])
-
-//   // const _data = getNestedObject(data, ['data', 'listLocations', 'items'])
-//   // _data?.sort(dynamicSort("locName"))
-
-//   return({
-//     data: _data,
-//     errors: errors,
-//     isValidating: isValidating,
-//   })
-
-// }
-
-/** 
- * Can be called whenever locationListSimple data is affected by a mutation.
- * Revalidation can be called anywhere, even when useLocationListSimple is not present.
- */
-export const revalidateLocationListSimple = () => {
-  mutate(
-    [listLocationsSimple, { limit: 1000 }], 
-    null, 
-    { revalidate: true}
-  )
-}
-
 
 /**
  * Produces a full list of locNicks/locNames.
@@ -396,99 +355,99 @@ export const revalidateLocationListFull = () => {
 }
 
 
-/**
- * Produces a more extensive list of attributes for a single location.
- * Includes product customization info which can be accessed individually,
- * or as part of the full 'data' return object
- * @param {string} locNick ID value for the desired location.
- * @param {boolean} shouldFetch Fetches data only when true.
- * @returns {{ data: object, templateProds: object, prodsNotAllowed: object, altPrices: object, altLeadTimes: object }} A single object representing the requested location.
- */
-export const useLocationDetails = (locNick, shouldFetch) => {
+// /**
+//  * Produces a more extensive list of attributes for a single location.
+//  * Includes product customization info which can be accessed individually,
+//  * or as part of the full 'data' return object
+//  * @param {string} locNick ID value for the desired location.
+//  * @param {boolean} shouldFetch Fetches data only when true.
+//  * @returns {{ data: object, templateProds: object, prodsNotAllowed: object, altPrices: object, altLeadTimes: object }} A single object representing the requested location.
+//  */
+// export const useLocationDetails = (locNick, shouldFetch) => {
 
-  const { data, errors, mutate, isValidating } = useSWR(
-    shouldFetch ? [getLocationDetails, { locNick: locNick }] : null, 
-    gqlFetcher, 
-    defaultSwrOptions
-  )
+//   const { data, errors, mutate, isValidating } = useSWR(
+//     shouldFetch ? [getLocationDetails, { locNick: locNick }] : null, 
+//     gqlFetcher, 
+//     defaultSwrOptions
+//   )
   
-  const _data = getNestedObject(data, ['data', 'getLocation'])
-  const _templateProds = getNestedObject(data, ['data', 'getLocation', 'templateProd', 'items'])
-  const _prodsNotAllowed = getNestedObject(data, ['data', 'getLocation', 'prodsNotAllowed', 'items'])
-  const _altPrices = getNestedObject(data, ['data', 'getLocation', 'customProd', 'items'])
-  const _altLeadTimes = getNestedObject(data, ['data', 'getLocation', 'altLeadTimeByProduct', 'items'])
+//   const _data = getNestedObject(data, ['data', 'getLocation'])
+//   const _templateProds = getNestedObject(data, ['data', 'getLocation', 'templateProd', 'items'])
+//   const _prodsNotAllowed = getNestedObject(data, ['data', 'getLocation', 'prodsNotAllowed', 'items'])
+//   const _altPrices = getNestedObject(data, ['data', 'getLocation', 'customProd', 'items'])
+//   const _altLeadTimes = getNestedObject(data, ['data', 'getLocation', 'altLeadTimeByProduct', 'items'])
 
-  return({ 
-    data: _data,
-    templateProds: _templateProds,
-    prodsNotAllowed: _prodsNotAllowed,
-    altPrices: _altPrices,
-    altLeadTimes: _altLeadTimes,
-    errors: errors,
-    mutate: mutate,
-    isValidating: isValidating
-  })
-}
+//   return({ 
+//     data: _data,
+//     templateProds: _templateProds,
+//     prodsNotAllowed: _prodsNotAllowed,
+//     altPrices: _altPrices,
+//     altLeadTimes: _altLeadTimes,
+//     errors: errors,
+//     mutate: mutate,
+//     isValidating: isValidating
+//   })
+// }
 
-/** 
- * Can be called whenever locationDetails data is affected by a mutation.
- * Revalidation can be called anywhere, even when useLocationDetails is not present.
- */
-export const revalidateLocationDetails = (locNick) => {
-  mutate(
-    [getLocationDetails, { locNick: locNick }],
-    null,
-    { revalidate: true }
-  )
-}
+// /** 
+//  * Can be called whenever locationDetails data is affected by a mutation.
+//  * Revalidation can be called anywhere, even when useLocationDetails is not present.
+//  */
+// export const revalidateLocationDetails = (locNick) => {
+//   mutate(
+//     [getLocationDetails, { locNick: locNick }],
+//     null,
+//     { revalidate: true }
+//   )
+// }
 
-// consider calling the revalidate functions above after 
-// performing one or a batch of mutations below.
-
-
-/*************
- * MUTATIONS *
- *************/
-
-const LOGGING = true
-
-export const createLocation = async (createLocationInput) => {
-  if (LOGGING) console.log("Create location input: ", createLocationInput)
-
-  if (!createLocationSchema.isValid(createLocationInput)) {
-    console.log("createLocation validation failed")
-    return
-  }
-
-  const response = await gqlFetcher([
-    createLocationMutation, 
-    { input: createLocationInput }
-  ])
-  if (LOGGING) console.log("Create location response: ", response)
-
-}
+// // consider calling the revalidate functions above after 
+// // performing one or a batch of mutations below.
 
 
-export const updateLocation = async (updateLocationInput) => {
-  if (LOGGING) console.log("Update location input: ", updateLocationInput)
-  const response = await gqlFetcher([
-    updateLocationMutation, 
-    { input: updateLocationInput }
-  ])
-  if (LOGGING) console.log("Update location response: ", response)
+// /*************
+//  * MUTATIONS *
+//  *************/
 
-}
+// const LOGGING = true
+
+// export const createLocation = async (createLocationInput) => {
+//   if (LOGGING) console.log("Create location input: ", createLocationInput)
+
+//   if (!createLocationSchema.isValid(createLocationInput)) {
+//     console.log("createLocation validation failed")
+//     return
+//   }
+
+//   const response = await gqlFetcher([
+//     createLocationMutation, 
+//     { input: createLocationInput }
+//   ])
+//   if (LOGGING) console.log("Create location response: ", response)
+
+// }
 
 
-export const deleteLocation = async (deleteLocationInput) => {
-  if (LOGGING) console.log("Delete location input: ", deleteLocationInput)
-  const response = await gqlFetcher([
-    deleteLocationMutation,
-    { input: deleteLocationInput }
-  ])
-  if (LOGGING) console.log("Delete location response: ", response)
+// export const updateLocation = async (updateLocationInput) => {
+//   if (LOGGING) console.log("Update location input: ", updateLocationInput)
+//   const response = await gqlFetcher([
+//     updateLocationMutation, 
+//     { input: updateLocationInput }
+//   ])
+//   if (LOGGING) console.log("Update location response: ", response)
 
-}
+// }
+
+
+// export const deleteLocation = async (deleteLocationInput) => {
+//   if (LOGGING) console.log("Delete location input: ", deleteLocationInput)
+//   const response = await gqlFetcher([
+//     deleteLocationMutation,
+//     { input: deleteLocationInput }
+//   ])
+//   if (LOGGING) console.log("Delete location response: ", response)
+
+// }
 
 
 /***********
--- src/swr.js
+++ src/swr.js
@@ -27,53 +27,53 @@ const fetcher = async (path) => {
   return res;
 };
 
-export function useLocUserList() {
-  const { data, error, mutate } = useSWR(
-    { url: "/locations/grabDetailedLocUserList" },
-    fetcher,
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: true,
-    }
-  );
+// export function useLocUserList() {
+//   const { data, error, mutate } = useSWR(
+//     { url: "/locations/grabDetailedLocUserList" },
+//     fetcher,
+//     {
+//       revalidateIfStale: false,
+//       revalidateOnFocus: false,
+//       revalidateOnReconnect: true,
+//     }
+//   );
 
-  return {
-    locationList: {
-      data: data ? sortAtoZDataByIndex(data.data.body.items, "locName") : data,
-      isLoading: !error && !data,
-      isError: error,
-      revalidate: () => mutate(),
-    },
-  };
-}
+//   return {
+//     locationList: {
+//       data: data ? sortAtoZDataByIndex(data.data.body.items, "locName") : data,
+//       isLoading: !error && !data,
+//       isError: error,
+//       revalidate: () => mutate(),
+//     },
+//   };
+// }
 
-export function useSimpleLocationList() {
-  const { data, error, mutate } = useSWR(
-    { url: "/locations/grabDetailedLocationList" },
-    fetcher,
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
+// export function useSimpleLocationList() {
+//   const { data, error, mutate } = useSWR(
+//     { url: "/locations/grabDetailedLocationList" },
+//     fetcher,
+//     {
+//       revalidateIfStale: false,
+//       revalidateOnFocus: false,
+//       revalidateOnReconnect: false,
+//     }
+//   );
 
-  return {
-    simpleLocationList: {
-      data: data
-        ? sortAtoZDataByIndex(data.data.body.items, "locName").map((loc) => ({
-            label: loc.locName,
-            value: loc.locNick,
+//   return {
+//     simpleLocationList: {
+//       data: data
+//         ? sortAtoZDataByIndex(data.data.body.items, "locName").map((loc) => ({
+//             label: loc.locName,
+//             value: loc.locNick,
            
-          }))
-        : data,
-      isLoading: !error && !data,
-      isError: error,
-      revalidate: () => mutate(),
-    },
-  };
-}
+//           }))
+//         : data,
+//       isLoading: !error && !data,
+//       isError: error,
+//       revalidate: () => mutate(),
+//     },
+//   };
+// }
 
 export function useProductList() {
   const { data, error, mutate } = useSWR(
