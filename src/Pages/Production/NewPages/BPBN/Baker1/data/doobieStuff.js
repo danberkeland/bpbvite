6d954e0739cbd0edefcaa594e5a01c663bcd97e6
Dan Berkeland <danberkeland@gmail.com>
2024-03-21 19:59:12 -0700

    doobie stuff

--- src/Pages/Production/NewPages/BPBN/Baker1/data/doobieStuff.js
+++ src/Pages/Production/NewPages/BPBN/Baker1/data/doobieStuff.js
@@ -28,13 +28,13 @@ export const useDoobieStuff = ({ reportDate }) => {
       Prod: "Doobie Buns",
       Bucket: "YES",
       Mix: yes ? "NO" : "NO",
-      Bake: yes ? "YES" : "YES",
+      Bake: yes ? "NO" : "NO",
     },
     {
       Prod: "Siciliano",
       Bucket: "YES",
-      Mix: yes ? "YES" : "YES",
-      Bake: yes ? "NO" : "NO",
+      Mix: yes ? "NO" : "NO",
+      Bake: yes ? "YES" : "YES",
     },
   ]
 
