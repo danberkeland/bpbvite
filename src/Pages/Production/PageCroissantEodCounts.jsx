42a05731f4f99bbfc4a72b94882a275092db1c5a
dkatosan <dkatosanma@gmail.com>
2024-04-15 23:22:31 -0700

    working on croix eod counts

--- src/Pages/Production/PageCroissantEodCounts.jsx
+++ src/Pages/Production/PageCroissantEodCounts.jsx
@@ -2,37 +2,182 @@ import { DataTable } from "primereact/datatable"
 import { DT } from "../../utils/dateTimeFns"
 import { Column } from "primereact/column"
 import { Button } from "primereact/button"
+import { useCroissantProduction } from "./useCroissantShapingData"
+import { useProducts } from "../../data/product/useProducts"
+import { keyBy } from "../../utils/collectionFns"
+import { useEffect, useMemo, useState } from "react"
+import { InputNumber } from "primereact/inputnumber"
 
+const eodProdNicks = ['ch', 'mb', 'mini', 'pg', 'pl', 'sf']
 
+const useCroissantEodData = ({ reportDT, shouldFetch }) => {
+  const { croixData } = useCroissantProduction({ reportDT, shouldFetch: true})
+  const { data:PRD } = useProducts({ shouldFetch })
+
+  const calculateCroissantEod = () => {
+    if (!croixData || !PRD) return { data: undefined }
+
+    const products = keyBy(PRD, P => P.prodNick)
+    const data = eodProdNicks.map(prodNick => {
+      
+      const {  freezerNorth, freezerNorthClosing } = products[prodNick]
+      const { freezerCount=0, sheetMake=0, batchSize=0, R0Cum=0 } = croixData.find(row => row.prodNick === prodNick) ?? {}
+
+      return {
+        prodNick,
+        freezerCount,
+        freezerClosing: sheetMake * batchSize + R0Cum,
+        freezerNorth,
+        freezerNorthClosing,
+      }
+    })
+
+    return { data }
+  }
+  
+  return useMemo(calculateCroissantEod, [croixData, PRD])
+
+}
 
 const PageCroissantEodCounts = () => {
   const reportDT = DT.today()
 
+  const { data:eodData } = useCroissantEodData({ reportDT, shouldFetch: true })
+  const [eodChanges, setEodChanges] = useState()
+  useEffect(() => {
+    if (!!eodData) setEodChanges(structuredClone(eodData))
+  }, [eodData])
+
+  const [editingAttribute, setEditingAttribute] = useState('')
+
+  console.log(eodData, eodChanges)
+
+  const submitChanges = () => {
+    if (!eodData) return
+    console.log(
+      eodChanges.filter((row, idx) => 
+        Object.keys(row).every(attribute => 
+          eodChanges[idx][attribute] === eodData[idx][attribute]
+        )
+      )
+    )
+  }
+
+  const ToggleEditButton = ({ attribute }) => editingAttribute === attribute 
+    ? <div style={{display: "flex", gap: "1rem"}}>
+        <Button icon="pi pi-times" className="p-button-rounded p-button-outlined" 
+          onClick={() => {
+            setEditingAttribute('')
+            setEodChanges(structuredClone(eodData))
+          }} 
+        />
+        <Button icon="pi pi-send"  className="p-button-rounded" 
+          onClick={() => submitChanges()}
+          disabled={!eodData || !eodChanges || eodData.every((row, idx) => row[attribute] === eodChanges[idx][attribute])}
+        />
+      </div>
+    : <Button 
+        label="Edit" 
+        onClick={() => setEditingAttribute(attribute)} 
+        disabled={0
+          || !eodData
+          || !eodChanges
+          || (!!editingAttribute && editingAttribute !== attribute)
+        } 
+      />
+
+  const countBodyTemplate = ({ attribute, relatedAttribute, idx }) => 
+    !!eodData && editingAttribute === attribute
+      ? <InputNumber 
+          value={eodChanges[idx][attribute]}
+          min={0}
+          max={9999}
+          onChange={e => {
+            let newState = structuredClone(eodChanges)
+            newState[idx][attribute] = e.value
+            newState[idx][relatedAttribute] = eodData[idx][relatedAttribute] + (e.value ?? 0) - eodData[idx][attribute]
+            setEodChanges(newState)
+          }}
+          onBlur={e => {
+            if (eodChanges[idx][attribute] === null) {
+              let newState = structuredClone(eodChanges)
+              newState[idx][attribute] = 0
+              newState[idx][relatedAttribute] = eodData[idx][relatedAttribute] - eodData[idx][attribute]
+              setEodChanges(newState)
+            }
+          }}
+          inputStyle={{width: "4rem"}}
+        />
+      : eodChanges[idx][attribute]
+  
+
   return (
-    <div style={{padding: "2rem 5rem 5rem 5rem", width:"52rem", margin: "auto"}}>
+    <div style={{padding: "2rem 5rem 5rem 5rem", width:"60rem", margin: "auto"}}>
       <h1>Croissant Freezer Count {reportDT.toFormat('MM/dd/yyyy')}</h1>
 
       <div style={{display: "flex", justifyContent: "space-between"}}>
         <div>
           <h2>South/Prado</h2>
           <DataTable
+            value={eodChanges}
             responsiveLayout="scroll"
             size="large"
           >
-            <Column header="Product"                         field="prodNick" />
-            <Column header={<span>Opening <br/>Count</span>} field="freezerCount"   footer={<Button label="Edit" />} />
-            <Column header={<span>Closing <br/>Count</span>} field="freezerClosing" footer={<Button label="Edit" />} />
+            <Column 
+              header="Product"
+              field="prodNick" 
+            />
+            <Column 
+              header={<span>Opening <br/>Count</span>} 
+              body={(_, options) => countBodyTemplate({ 
+                attribute: 'freezerCount',
+                relatedAttribute: 'freezerClosing', 
+                idx: options.rowIndex, 
+              })}
+              footer={ToggleEditButton({ attribute: 'freezerCount' })} 
+              style={{width: "8rem", padding: "0"}}
+              footerStyle={{paddingBlock: "1rem"}}
+            />
+            <Column 
+              header={<span>Closing <br/>Count</span>} 
+              body={(_, options) => countBodyTemplate({ 
+                attribute: 'freezerClosing', 
+                relatedAttribute: 'freezerCount',
+                idx: options.rowIndex 
+              })}
+              footer={ToggleEditButton({ attribute: 'freezerClosing' })}
+              style={{width: "8rem", padding: "0"}}
+              footerStyle={{paddingBlock: "1rem"}}
+            />
           </DataTable>
         </div>
         <div>
           <h2>North/Carlton</h2>
           <DataTable
+            value={eodChanges}
             responsiveLayout="scroll"
             size="large"
           >
-            <Column header="Product"                         field="prodNick" />
-            <Column header={<span>Opening <br/>Count</span>} field="freezerNorth" />
-            <Column header={<span>Closing <br/>Count</span>} field="freezerNorthClosing" footer={<Button label="Edit" />} />
+            <Column 
+              header="Product" 
+              field="prodNick" 
+            />
+            <Column 
+              header={<span>Opening <br/>Count</span>} 
+              field="freezerNorth" 
+              style={{width: "8rem", padding: "0"}}
+            />
+            <Column 
+              header={<span>Closing <br/>Count</span>} 
+              body={(_, options) => countBodyTemplate({ 
+                attribute: 'freezerNorthClosing', 
+                relatedAttribute: 'freezerNorth',
+                idx: options.rowIndex 
+              })}
+              footer={ToggleEditButton({ attribute: 'freezerNorthClosing' })}
+              style={{width: "8rem", padding: "0"}}
+              footerStyle={{paddingBlock: "1rem"}}
+            />
           </DataTable>
         </div>
       </div>
