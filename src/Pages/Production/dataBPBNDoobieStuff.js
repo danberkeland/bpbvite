4c8201528a0e1727d6a1c85685d71d807800c06a
Dan Berkeland <danberkeland@gmail.com>
2024-05-03 19:53:15 -0700

    doobie stuff

--- src/Pages/Production/dataBPBNDoobieStuff.js
+++ src/Pages/Production/dataBPBNDoobieStuff.js
@@ -7,22 +7,22 @@ import { DateTime } from "luxon"
 export const useDoobieStuff = ({ reportDT }) => {
   let yes = reportDT.ordinal % 2 === 0
   
-  const doobieStuffx = [
+  const doobieStuff = [
     {
       Prod: "Doobie Buns",
       Bucket: "YES",
       Mix: yes ? "NO" : "NO",
-      Bake: yes ? "NO" : "NO",
+      Bake: yes ? "YES" : "YES",
     },
     {
       Prod: "Siciliano",
       Bucket: "YES",
       Mix: yes ? "YES" : "YES",
-      Bake: yes ? "YES" : "YES",
+      Bake: yes ? "NO" : "NO",
     },
   ]
 
-  const doobieStuff = [
+  const doobieStuffx = [
      {
        Prod: "Doobie Buns",
        Bucket: "YES",
