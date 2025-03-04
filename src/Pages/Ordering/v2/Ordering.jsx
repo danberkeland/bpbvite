c7e5b67bc893e25b98dc65d61b6d64e186b8861f
Dan Berkeland <danberkeland@gmail.com>
2025-03-04 09:23:24 -0800

    test

--- src/Pages/Ordering/v2/Ordering.jsx
+++ src/Pages/Ordering/v2/Ordering.jsx
@@ -15,7 +15,7 @@ import { useOrderingPageData } from "./orderingPageData.js"
 import { DBLocation } from "../../../data/types.d.jsx"
 import { useWindowSize } from "../../../utils/useWindowSize.js"
 
-// import "./ordering.css"
+// import "./ordering.css  "
 import { DateObj } from "./orderingTypes.d.js"
 import { getSelectedDateList } from "./orderingPageCalcs.js"
 import { isEqual } from "lodash"
