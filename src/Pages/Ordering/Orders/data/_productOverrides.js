cc59298acfc4298d0b4638ca7cb39bae83c14c42
David Kato <dkatosanma@gmail.com>
2025-10-23 23:39:53 -0700

    remove hardcoded overrides

--- src/Pages/Ordering/Orders/data/_productOverrides.js
+++ src/Pages/Ordering/Orders/data/_productOverrides.js
@@ -62,61 +62,61 @@ export const applyOverridesForRouteAssignment = ({
 
 /** test behavior with different attribute settings before committing */
 export const preDBOverrides = {
-  fr:	   { leadTime: 1 },                  // french
-  rfr:	 { leadTime: 1 },
-  cub:	 { leadTime: 1 },
-  mcub:	 { leadTime: 1 },
-  ses:	 { leadTime: 2 },
-  zdog:	 { leadTime: 1 },
-  bz:	   { leadTime: 1 },
+  // fr:	   { leadTime: 1 },                  // french
+  // rfr:	 { leadTime: 1 },
+  // cub:	 { leadTime: 1 },
+  // mcub:	 { leadTime: 1 },
+  // ses:	 { leadTime: 2 },
+  // zdog:	 { leadTime: 1 },
+  // bz:	   { leadTime: 1 },
 
-  lgbz:	 { leadTime: 1 },                  // brioche
-  brsl:	 { leadTime: 1 },
-  bri:	 { leadTime: 1 },
+  // lgbz:	 { leadTime: 1 },                  // brioche
+  // brsl:	 { leadTime: 1 },
+  // bri:	 { leadTime: 1 },
 
-  wwbz:	 { leadTime: 2 },                  // WW
+  // wwbz:	 { leadTime: 2 },                  // WW
 
-  hok:	 { leadTime: 2 },                  // doobie
+  // hok:	 { leadTime: 2 },                  // doobie
 
-  sic:	 { leadTime: 1 },                  // sic
+  // sic:	 { leadTime: 1 },                  // sic
 
-  foc:	 { leadTime: 2 },                  // focaccias
-  hfoc:	 { leadTime: 2 },
+  // foc:	 { leadTime: 2 },                  // focaccias
+  // hfoc:	 { leadTime: 2 },
 
-  smpz:	 { leadTime: 2, readyTime: 8.2 },  // pretzel
-  lgpz:	 { leadTime: 2, readyTime: 8.2 },
-  pzb:	 { leadTime: 2, readyTime: 8.2 },
-  pzsl:	 { leadTime: 2, readyTime: 8.2 },
-  ptz:	 { leadTime: 2, readyTime: 8.2 },
-  pzst:	 { leadTime: 2, readyTime: 8.2 },
-  unpz:	 { leadTime: 2, readyTime: 8.2 },
+  // smpz:	 { leadTime: 2, readyTime: 8.2 },  // pretzel
+  // lgpz:	 { leadTime: 2, readyTime: 8.2 },
+  // pzb:	 { leadTime: 2, readyTime: 8.2 },
+  // pzsl:	 { leadTime: 2, readyTime: 8.2 },
+  // ptz:	 { leadTime: 2, readyTime: 8.2 },
+  // pzst:	 { leadTime: 2, readyTime: 8.2 },
+  // unpz:	 { leadTime: 2, readyTime: 8.2 },
 
-  frpl:  { leadTime: 1, readyTime: 20  },  // frozen croix
-  frch:  { leadTime: 1, readyTime: 20  },
-  frpg:  { leadTime: 1, readyTime: 20  },
-  frsf:  { leadTime: 1, readyTime: 20  },
-  frmni: { leadTime: 1, readyTime: 20  },
-  fral:  { leadTime: 1, readyTime: 20  },
+  // frpl:  { leadTime: 1, readyTime: 20  },  // frozen croix
+  // frch:  { leadTime: 1, readyTime: 20  },
+  // frpg:  { leadTime: 1, readyTime: 20  },
+  // frsf:  { leadTime: 1, readyTime: 20  },
+  // frmni: { leadTime: 1, readyTime: 20  },
+  // fral:  { leadTime: 1, readyTime: 20  },
 
-  bb:    { readyTime: 5 },                 // non-croix baked pastries
-  sco:   { readyTime: 5 },
-  bd:    { readyTime: 5 },
-  brn:   { readyTime: 5 },
+  // bb:    { readyTime: 5 },                 // non-croix baked pastries
+  // sco:   { readyTime: 5 },
+  // bd:    { readyTime: 5 },
+  // brn:   { readyTime: 5 },
 
-  bdrd:  { isWhole: true },
+  // bdrd:  { isWhole: true },
 
 }
 
 // for mocking up test values for standard db overrides ("overrides of overrides")
 export const postDBOverrides = {
-  "high#lgbz": { leadTime: 0 },
-  "high#wwbz": { leadTime: 0 },
-  'high#sic': { leadTime: 0 },
-  'high#hok': { leadTime: 0 },
+  // "high#lgbz": { leadTime: 0 },
+  // "high#wwbz": { leadTime: 0 },
+  // 'high#sic': { leadTime: 0 },
+  // 'high#hok': { leadTime: 0 },
 
-  'hios#hok': { leadTime: 0 },
-  'hios#sic': { leadTime: 0 },
-  'hios#wwbz': { leadTime: 0 },
+  // 'hios#hok': { leadTime: 0 },
+  // 'hios#sic': { leadTime: 0 },
+  // 'hios#wwbz': { leadTime: 0 },
 }
 
 // changing carlton pickup duration so that it properly overlaps with
@@ -127,9 +127,9 @@ export const getRouteOverridesForAssignment = ({
   route 
 }) => {
   let overrides = {}
-  if (route?.routeNick === "Pick up Carlton") {
-    overrides = { routeStart: 6.75, routeTime: 4}
-  }
+  // if (route?.routeNick === "Pick up Carlton") {
+  //   overrides = { routeStart: 6.75, routeTime: 4}
+  // }
 
   return overrides
 }
